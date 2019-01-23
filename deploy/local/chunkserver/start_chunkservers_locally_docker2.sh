5a5779dd2882e5c6bb26d66fff3f50de53a77692
hzluozewen <hzluozewen@corp.netease.com>
2019-01-23 17:18:38 +0800

    use docker do test
    Change-Id: Icee249cc807478f0bdebedabf04727b3e263efa4

---
+++ deploy/local/chunkserver/start_chunkservers_locally_docker2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+home=/curve/deploy/local/chunkserver
+#home=.
+conf=${home}/conf
+log=${home}/log
+bin=/curve/bazel-bin/src/chunkserver
+#bin=.
+
+${bin}/chunkserver -bthread_concurrency=18 -raft_max_segment_size=8388608 -raft_sync=true -minloglevel=0 -conf=${conf}/chunkserver.conf.docker2 > ${log}/chunkserver.log.1 2>&1 &
