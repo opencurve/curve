e83409aa1e961fdbc14a692c2f76e88e315caea3
Wenyu Zhou <zhouwenyu01@corp.netease.com>
2019-04-19 11:48:59 +0800

    Change task execution callback mode to polling mode in chunkserver heartbeat
    Change-Id: I0267d431f50298ba57d275df2928949598af5266

--- src/chunkserver/heartbeat.h
+++ src/chunkserver/heartbeat.h
@@ -31,13 +31,7 @@ using ConfigChangeInfo  = curve::mds::heartbeat::ConfigChangeInfo;
 using CopysetConf       = curve::mds::heartbeat::CopysetConf;
 using CandidateError    = curve::mds::heartbeat::CandidateError;
 using TaskStatus        = butil::Status;
-
-class HeartbeatTask;
-class CopysetInfo;
-
-using CopysetNodePtr            = std::shared_ptr<CopysetNode>;
-using CopysetInfoPtr            = std::shared_ptr<CopysetInfo>;
-using HeartbeatTaskPtr          = std::shared_ptr<HeartbeatTask>;
+using CopysetNodePtr    = std::shared_ptr<CopysetNode>;
 
 /**
  * 心跳子系统选项
@@ -55,272 +49,6 @@ struct HeartbeatOptions {
     CopysetNodeManager*     copysetNodeManager;
 
     std::shared_ptr<LocalFileSystem> fs;
-};
-
-/**
- * 心跳任务类型
- */
-enum TASK_TYPE {
-    TASK_TYPE_ADD_PEER,
-    TASK_TYPE_TRANSFER_LEADER,
-    TASK_TYPE_REMOVE_PEER,
-    TASK_TYPE_CLEAN_PEER,
-    TASK_TYPE_NONE,
-};
-
-/**
- * 心跳任务处理类
- */
-class HeartbeatTask {
- public:
-    HeartbeatTask() {}
-    ~HeartbeatTask() {}
-
-    /**
-     * @brief 初始化心跳任务
-     * @param[in] type 任务类型
-     * @param[in] peerId 心跳任务的目标成员
-     */
-    void NewTask(TASK_TYPE type, const PeerId& peerId);
-
-    /**
-     * @brief 获取心跳任务所针对的目标成员
-     * @return 目标成员的ID
-     */
-    PeerId& GetPeerId();
-
-    /**
-     * @brief 获取心跳任务的类型
-     * @return 任务类型
-     */
-    TASK_TYPE GetType();
-
-    /**
-     * @brief 设置心跳任务的状态
-     * @param[in] status 目标状态
-     */
-    void SetStatus(const TaskStatus& status);
-
-    /**
-     * @brief 获取心跳任务的状态
-     * @return 任务状态
-     */
-    TaskStatus& GetStatus();
-
- private:
-    /*
-     * 任务类型
-     */
-    TASK_TYPE                       type_;
-
-    /*
-     * 任务的目标对象成员ID
-     */
-    PeerId                          peerId_;
-
-    /*
-     * 任务状态
-     */
-    TaskStatus                      status_;
-};
-
-/**
- * 复制组信息类,封装心跳系统所用到的各种复制组信息和接口
- */
-class CopysetInfo : public std::enable_shared_from_this<CopysetInfo> {
- public:
-    CopysetInfo(LogicPoolID poolId, CopysetID copysetId);
-    ~CopysetInfo();
-
-    /**
-     * @brief 初始化心跳任务
-     * @param[in] type 任务类型
-     * @param[in] peerId 心跳任务的目标成员
-     */
-    void NewTask(TASK_TYPE type, const PeerId& peerId);
-
-    /**
-     * @brief 结束心跳任务
-     * @param[in] status 任务将被设置的状态
-     */
-    void FinishTask(const TaskStatus& status);
-
-    /**
-     * @brief 判断复制组是否为Leader
-     * @return 返回是否为Leader
-     */
-    bool IsLeader();
-
-    /**
-     * @brief 获取复制组的Leader
-     * @return 复制组Leader的ID
-     */
-    int GetLeader(PeerId* leader);
-
-    /**
-     * @brief 获取复制组的Raft实现实例
-     * @param[out] node 出参,复制组实例的引用
-     * @return 0:成功，非0失败
-     */
-    int GetNode(scoped_refptr<braft::NodeImpl>* node);
-
-    /**
-     * @brief 获取复制组的成员列表
-     * @param[out] peers 出参,复制组成员列表
-     * @return 0:成功，非0失败
-     */
-    int ListPeers(std::vector<PeerId>* peers);
-
-    /**
-     * @brief 切换复制组的Leader
-     * @param[in] peerId 目标Leader的成员ID
-     * @return 心跳任务的引用
-     */
-    TaskStatus TransferLeader(const PeerId& peerId);
-
-    /**
-     * @brief 复制组添加新成员
-     * @param[in] peerId 新成员的ID
-     * @return 心跳任务的引用
-     */
-    TaskStatus AddPeer(const PeerId& peerId);
-
-    /**
-     * @brief 复制组删除成员
-     * @param[in] peerId 将要删除成员的ID
-     * @return 心跳任务的引用
-     */
-    TaskStatus RemovePeer(const PeerId& peerId);
-
-    /**
-     * @brief 清理复制组实例及持久化数据
-     * @return 心跳任务的引用
-     */
-    TaskStatus CleanPeer();
-
-    /**
-     * @brief 获取复制组ID
-     * @return 复制组ID
-     */
-    CopysetID GetCopysetId();
-
-    /**
-     * @brief 获取复制组所在的逻辑池的ID
-     * @return 复制组所在的逻辑池的ID
-     */
-    LogicPoolID GetLogicPoolId();
-
-    /**
-     * @brief 获取复制组的组ID
-     * @return 心复制组的组ID
-     */
-    GroupNid GetGroupId();
-
-    /**
-     * @brief 获取复制组活跃任务的状态
-     * @return 任务状态
-     */
-    TaskStatus& GetStatus();
-
-    /**
-     * @brief 获取复制组活跃任务的目标成员
-     * @return 复制组活跃任务的目标成员
-     */
-    PeerId& GetPeerInTask();
-
-    /**
-     * @brief 获取复制组的运行实例
-     * @return 复制组实例的引用
-     */
-    CopysetNodePtr GetCopysetNode();
-
-    /**
-     * @brief 设置复制组的运行实例
-     * @param[in] copyset 复制组实例的引用
-     */
-    void SetCopysetNode(CopysetNodePtr copyset);
-
-    /**
-     * @brief 判断复制组的任务是否结束
-     */
-    bool IsTaskOngoing();
-
-    /**
-     * @brief 判断复制组是否有正在进行的任务
-     */
-    bool HasTask();
-
-    /**
-     * @brief 清理复制组的心跳任务信息
-     */
-    void ReleaseTask();
-
-    /**
-     * @brief 更新复制组信息的心跳周期，根据跟全局最新心跳周期比较可判断复制组
-     * 是否活跃, 以清理过时的复制组信息
-     * @param[in] term 将要给复制组信息设置的目标心跳周期
-     */
-    void UpdateTerm(uint64_t term);
-
-    /**
-     * @brief 获取复制组信息的心跳周期
-     * @return 心跳周期
-     */
-    uint64_t GetTerm();
-
-    /**
-     * @brief 获取复制组实例的epoch
-     * @return 复制组实例的epoch
-     */
-    uint64_t GetEpoch();
-
-    /**
-     * @brief 设置复制组管理器
-     * @param[in] man 复制组管理器的引用
-     */
-    static void SetCopysetNodeManager(CopysetNodeManager* man);
-
- private:
-    /*
-     * Copyset逻辑池ID
-     */
-    LogicPoolID             poolId_;
-
-    /*
-     * Copyset ID
-     */
-    CopysetID               copysetId_;
-
-    /*
-     * CopysetInfo对应的Copyset实例引用
-     */
-    CopysetNodePtr          copyset_;
-
-    /*
-     * CopysetInfo当前的心跳周期号，每个心跳一个term号，跟raft的term没有关系，
-     * 用来区别活跃与非活跃Copyset跟踪项
-     */
-    uint64_t                term_;
-
-    /*
-     * Copyset是否有任务在进行
-     */
-    std::atomic<bool>       taskOngoing_;
-
-    /*
-     * Copyset是否存在任务
-     */
-    std::atomic<bool>       newTask_;
-
-    /*
-     * Copyset对应的任务实例引用
-     */
-    HeartbeatTaskPtr        task_;
-
-    /*
-     * Copyset管理模块引用
-     */
-    static CopysetNodeManager*  copysetNodeManager_;
 };
 
 /**
@@ -350,37 +78,17 @@ class Heartbeat {
      */
     int Run();
 
+ private:
     /**
      * @brief 停止心跳子系统
      * @return 0:成功，非0失败
      */
     int Stop();
 
- private:
     /*
      * 心跳工作线程
      */
     static void HeartbeatWorker(Heartbeat *heartbeat);
-
-    /*
-     * 更新Copyset当前状态信息到Copyset信息表
-     */
-    int UpdateCopysetInfo(const std::vector<CopysetNodePtr>& copysets);
-
-    /*
-     * 清理Copyset信息表中非活跃的Copyset信息项
-     */
-    void CleanAgingCopysetInfo();
-
-    /*
-     * 获取指定的Copyset信息项目
-     */
-    CopysetInfoPtr GetCopysetInfo(LogicPoolID poolId, CopysetID copysetId);
-
-    /*
-     * 获取指定的Copyset信息项目
-     */
-    CopysetInfoPtr GetCopysetInfo(CopysetNodePtr copyset);
 
     /*
      * 获取Chunkserver存储空间信息
@@ -425,6 +133,12 @@ class Heartbeat {
     void DumpHeartbeatResponse(const HeartbeatResponse& response);
 
     /*
+     * 清理复制组实例及持久化数据
+     */
+    TaskStatus PurgeCopyset(LogicPoolID poolId, CopysetID copysetId);
+
+ private:
+    /*
      * 心跳线程
      */
     std::unique_ptr<std::thread>    hbThread_;
@@ -438,16 +152,6 @@ class Heartbeat {
      * 控制心跳模块运行或停止
      */
     std::atomic<bool>               toStop_;
-
-    /*
-     * 心跳模块的当前心跳周期
-     */
-    uint64_t                        term_;
-
-    /*
-     * 当前的复制组Leader角色数量
-     */
-    int32_t                         leaders_;
 
     /*
      * MDS的地址
@@ -465,9 +169,9 @@ class Heartbeat {
     std::string                     dataDirPath_;
 
     /*
-     * Copyset信息列表
+     * Copyset管理模块
      */
-    std::map<GroupNid, CopysetInfoPtr>  copysets_;
+    CopysetNodeManager*             copysetMan_;
 };
 
 }  // namespace chunkserver
