a35d6fc693d4d4d4c8302d0d95ad1a1b07f6cfdf
lixiaocui1 <lixiaocui1@corp.netease.com>
2019-05-22 10:23:36 +0800

    chunkserver在有copyset的情况下重启失败
    Change-Id: I40aad580f32af249fe0dcf5389dd9a8d10a7c76b

--- test/chunkserver/heartbeat_test_main.cpp
+++ test/chunkserver/heartbeat_test_main.cpp
@@ -15,28 +15,16 @@
 #include "src/chunkserver/heartbeat.h"
 #include "test/chunkserver/heartbeat_test.h"
 
-static char* param[3][6] = {
+static char* param[3][2] = {
     {
-        "bazel-bin/test/chunkserver/chunkserver_test",
-        "-bthread_concurrency=18",
-        "-raft_max_segment_size=8388608",
-        "-raft_sync=true",
         "-minloglevel=2",
         "-conf=deploy/local/chunkserver/conf/chunkserver.conf.0",
     },
     {
-        "bazel-bin/test/chunkserver/chunkserver_test",
-        "-bthread_concurrency=18",
-        "-raft_max_segment_size=8388608",
-        "-raft_sync=true",
         "-minloglevel=2",
         "-conf=deploy/local/chunkserver/conf/chunkserver.conf.1",
     },
     {
-        "bazel-bin/test/chunkserver/chunkserver_test",
-        "-bthread_concurrency=18",
-        "-raft_max_segment_size=8388608",
-        "-raft_sync=true",
         "-minloglevel=2",
         "-conf=deploy/local/chunkserver/conf/chunkserver.conf.2",
     },
@@ -44,38 +32,21 @@ static char* param[3][6] = {
 
 using ::curve::chunkserver::ChunkServer;
 
-struct ChunkServerProcess {
-    int id;
-    ChunkServer* chunkserver;
-    std::thread csThread;
-};
-
 butil::AtExitManager atExitManager;
 
-static void* RunChunkServer(
-    ChunkServer *chunkserver, int argc, char** argv) {
-    int ret = chunkserver->Run(argc, argv);
-    LOG_IF(ERROR, ret != 0) << "Failed to run chunkserver process";
-    return NULL;
-}
-
-static ChunkServerProcess StartChunkServer(int i, int argc, char** argv) {
-    ChunkServerProcess proc;
-    proc.id = i;
-
-    proc.chunkserver = new curve::chunkserver::ChunkServer();
-    if (proc.chunkserver == nullptr) {
+static int RunChunkServer(int i, int argc, char** argv) {
+    auto chunkserver = new curve::chunkserver::ChunkServer();
+    if (chunkserver == nullptr) {
         LOG(ERROR) << "Failed to create chunkserver " << i;
-        return proc;
+        return -1;
+    }
+    int ret = chunkserver->Run(argc, argv);
+    if (ret < 0) {
+        LOG(ERROR) << "Failed to run chunkserver process " << ret;
+        return -1;
     }
 
-    proc.csThread = std::thread(RunChunkServer, proc.chunkserver, argc, argv);
-    return proc;
-}
-
-static void StopChunkServer(ChunkServerProcess *proc) {
-    proc->csThread.join();
-    LOG(ERROR) << "stop chunkserver ok.";
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -87,54 +58,61 @@ int main(int argc, char* argv[]) {
     LOG_IF(ERROR, 0 != curve::chunkserver::RemovePeersData(true))
         << "Failed to remove peers' data";
 
-    // chunkserver proccess 0
     pids[0] = fork();
     if (pids[0] < 0) {
         LOG(FATAL) << "Failed to create chunkserver process 0";
     } else if (pids[0] == 0) {
-        ChunkServerProcess proc =
-            StartChunkServer(0, sizeof(param[0])/sizeof(char *), param[0]);
-        StopChunkServer(&proc);
-        LOG(INFO) << "chunkserver-0 stopped";
-        return 0;
+        return RunChunkServer(0, sizeof(param[0])/sizeof(char *), param[0]);
     }
 
-    // chunkserver proccess 1
     pids[1] = fork();
     if (pids[1] < 0) {
         LOG(FATAL) << "Failed to create chunkserver process 1";
     } else if (pids[1] == 0) {
-        ChunkServerProcess proc =
-            StartChunkServer(1, sizeof(param[1])/sizeof(char *), param[1]);
-        StopChunkServer(&proc);
-        LOG(INFO) << "chunkserver-1 stopped";
-        return 0;
+        return RunChunkServer(1, sizeof(param[1])/sizeof(char *), param[1]);
     }
 
-    // chunkserver proccess 2
     pids[2] = fork();
     if (pids[2] < 0) {
         LOG(FATAL) << "Failed to create chunkserver process 2";
     } else if (pids[2] == 0) {
-        ChunkServerProcess proc =
-            StartChunkServer(2, sizeof(param[2])/sizeof(char *), param[2]);
-        StopChunkServer(&proc);
-        LOG(INFO) << "chunkserver-2 stopped";
-        return 0;
+        return RunChunkServer(2, sizeof(param[2])/sizeof(char *), param[2]);
     }
 
     // main test process
     {
-        ret = RUN_ALL_TESTS();
+        LOG(INFO) << "Run all test...";
+        pid_t pid = fork();
+        if (pid < 0) {
+            LOG(FATAL) << "Failed to create test proccess";
+        } else if (pid == 0) {
+            ret = RUN_ALL_TESTS();
+            return ret;
+        }
+        waitpid(pid, &ret, 0);
+        LOG(INFO) << "Run all test end... Return code: " << ret;
 
-        LOG(INFO) << "Test finished with code " << ret;
+        LOG(INFO) << "Stop all chunkserver";
         for (int i = 0; i < 3; i++) {
             kill(pids[i], SIGINT);
             waitpid(pids[i], &ret, 0);
-            LOG(INFO) << "Wait chunkserver " << i << " quiting: " << ret;
         }
-        LOG(INFO) << "Heartbeat testing finished.";
+        LOG(INFO) << "Stop all chunkserver success";
 
+        LOG(INFO) << "Restart chunkserver 1";
+        pid = fork();
+        if (pid < 0) {
+            LOG(FATAL) << "Failed to restart chunkserver process 1";
+        } else if (pid == 0) {
+            ret = RunChunkServer(1, sizeof(param[1])/sizeof(char *), param[1]);
+            return ret;
+        }
+        sleep(2);
+        kill(pid, SIGINT);
+        waitpid(pid, &ret, 0);
+        LOG(INFO) << "Stop restart chunkserver 1 ok. Return code: " << ret;
+
+        LOG(INFO) << "Heartbeat testing finished.";
         return ret;
     }
 }
