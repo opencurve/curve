4f67e16d2deceeb0db1717a3585ec47a764ee6c8
yangyaokai <yangyaokai@corp.netease.com>
2019-06-12 20:39:37 +0800

    add chunkserver metric
    Change-Id: I55533d2bd66fa164120a75201416ead3afc1b2e8

---
+++ src/chunkserver/passive_getfn.h
@@ -0,0 +1,46 @@
+/*
+ * Project: curve
+ * Created Date: Tuesday June 18th 2019
+ * Author: yangyaokai
+ * Copyright (c) 2019 netease
+ */
+
+#ifndef SRC_CHUNKSERVER_PASSIVE_GETFN_H_
+#define SRC_CHUNKSERVER_PASSIVE_GETFN_H_
+
+#include "src/chunkserver/copyset_node_manager.h"
+#include "src/chunkserver/datastore/chunkfile_pool.h"
+
+namespace curve {
+namespace chunkserver {
+
+    /**
+     * 获取chunkserver上copyset数量
+     * @param arg: copyset node manager的对象指针
+     */
+    uint32_t getCopysetCountFunc(void* arg);
+    /**
+     * 获取chunkserver上leader数量
+     * @param arg: copyset node manager的对象指针
+     */
+    uint32_t getLeaderCountFunc(void* arg);
+    /**
+     * 获取chunkfilepool中剩余chunk的数量
+     * @param arg: chunkfilepool的对象指针
+     */
+    uint32_t getChunkLeftFunc(void* arg);
+    /**
+     * 获取datastore中chunk文件的数量
+     * @param arg: datastore的对象指针
+     */
+    uint32_t getDatastoreChunkCountFunc(void* arg);
+    /**
+     * 获取datastore中快照的数量
+     * @param arg: datastore的对象指针
+     */
+    uint32_t getDatastoreSnapshotCountFunc(void* arg);
+
+}  // namespace chunkserver
+}  // namespace curve
+
+#endif  // SRC_CHUNKSERVER_PASSIVE_GETFN_H_
