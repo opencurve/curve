7f49d39cecb4f576e494b984f909730d437f9206
lixiaocui1 <lixiaocui1@corp.netease.com>
2019-06-24 11:21:43 +0800

    mds高可用
    Change-Id: Ice58b078a880dd08f0e8283375b77ed195195a05

---
+++ test/mds/mock/mock_etcdclient.h
@@ -0,0 +1,46 @@
+/*
+ * Project: curve
+ * Created Date: 20190709
+ * Author: lixiaocui
+ * Copyright (c) 2018 netease
+ */
+
+#ifndef  TEST_MDS_MOCK_MOCK_ETCDCLIENT_H_
+#define  TEST_MDS_MOCK_MOCK_ETCDCLIENT_H_
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+#include "src/mds/nameserver2/etcd_client.h"
+#include "src/mds/nameserver2/namespace_storage_cache.h"
+
+namespace curve {
+namespace mds {
+class MockEtcdClient : public EtcdClientImp {
+ public:
+    virtual ~MockEtcdClient() {}
+    MOCK_METHOD2(Put, int(const std::string&, const std::string&));
+    MOCK_METHOD2(Get, int(const std::string&, std::string*));
+    MOCK_METHOD3(List,
+        int(const std::string&, const std::string&, std::vector<std::string>*));
+    MOCK_METHOD1(Delete, int(const std::string&));
+    MOCK_METHOD1(TxnN, int(const std::vector<Operation>&));
+    MOCK_METHOD3(CompareAndSwap, int(const std::string&, const std::string&,
+        const std::string&));
+    MOCK_METHOD5(CampaignLeader, int(const std::string&, const std::string&,
+        uint32_t, uint32_t, uint64_t*));
+    MOCK_METHOD3(LeaderObserve, int(uint64_t, uint64_t, const std::string&));
+};
+
+class MockLRUCache : public LRUCache {
+ public:
+    virtual ~MockLRUCache() {}
+    MOCK_METHOD2(Put, void(const std::string&, const std::string&));
+    MOCK_METHOD2(Get, bool(const std::string&, std::string*));
+    MOCK_METHOD1(Remove, void(const std::string&));
+};
+}  // namespace mds
+}  // namespace curve
+
+#endif  // TEST_MDS_MOCK_MOCK_ETCDCLIENT_H_
