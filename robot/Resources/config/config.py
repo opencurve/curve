4dc4d4747b6e275bc5ed2864976c61e38773dd12
chenyunhui <chenyunhui@corp.netease.com>
2019-07-10 14:27:50 +0800

    add abnormal test
    Change-Id: I758a2dc548a9da2ffb1760f8f4944cb86f9a758c
    (cherry picked from commit 342e0075f4c016c8ea72342facac105c745f9730)

---
+++ robot/Resources/config/config.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+'''
+基本配置信息,若配置新的环境，需要更改此文件
+'''
+#curve info
+curve_workspace = "/var/lib/jenkins/workspace/curve/curve_multijob/"
+chunkserver_start_script = "./deploy/local/chunkserver/start_chunkservers_locally.sh"
+chunkserver_stop_script = "./deploy/local/chunkserver/stop_chunkservers_locally.sh"
+mds_start = curve_workspace + "bazel-bin/src/mds/main/curvemds"
+snapshot_server_start = curve_workspace + "bazel-bin/src/snapshotcloneserver/snapshotcloneserver"
+curvefs_tool = curve_workspace + "bazel-bin/tools/curvefsTool"
+createfile_tool = curve_workspace + "bazel-bin/src/tools/createFile"
+libcurve_workflow = curve_workspace + "bazel-bin/test/client/fake/curve_client_workflow"
+chunkserver_log_dir = curve_workspace + "deploy/local/chunkserver/log/"
+snapshot_clone_server_conf = curve_workspace + "conf/snapshot_clone_server.conf"
+client_conf = curve_workspace + "conf/client.conf"
+
+# volume info
+default_vol_size = 5
+default_vol_max = 4000
+default_vol_extend_size = 10
+
+# file info
+file_name = "/1"
+old_name = "/1"
+new_name = "/2"
+file_name_with_parent = "/lzw/1"
+old_name_with_parent = "/lzw/1"
+new_name_with_parent = "/lzw/2"
+file_name_no_parent = "/xyz/test"
+dir_path = "/lzw"
+dir_path_with_parent = "/lzw/test"
+dir_path_no_parent = "/abc/test"
+user_name = "userinfo"
+pass_word = ""
+root_name = "root"
+root_password = "root_password"
+root_error_password = "root_error_password"
+size = 10737418240
+small_size = 1073741824
+big_size = 5497558138880
+new_size = 21474836480
+buf = "aaaaaaaa"*512
+buf_list = []
+offset = 0
+length = 4096
+write_stopped = False
+# mds info
+mds_ip = "127.0.0.1"
+mds_port = 6666
+mds_listen = "-listenAddr=127.0.0.1:6666"
+
+#abnomal test
+chunkserver_list = ["10.182.26.16","10.182.26.17","10.182.26.18","10.182.26.46","10.182.26.47","10.182.26.48","10.182.26.49","10.182.26.50","10.182.26.51"]
+mds_list = ["10.182.26.25"]
+etcd_list = ["10.182.26.25"]
+client_list = ["10.182.26.25"]
+abnormal_user = "nbs"
+pravie_key_path = "/home/nbs/rsa/id_rsa"
+abnormal_db_host = "10.182.26.25"
+recover_time = 10800
+offline_timeout = 60
+vol_uuid = ""
+
+# db info
+db_host = "127.0.0.1"
+db_port = 3306
+db_user = "root"
+db_pass = "qwer"
+mds_db_name = "curve_mds"
+snap_db_name = "curve_snapshot"
+
+# chunkserver mount point
+cs_0 = curve_workspace + "0"
+cs_1 = curve_workspace + "1"
+cs_2 = curve_workspace + "2"
+cs_num = 3
+# chunkserver log dir
+cs_log = "deploy/local/chunkserver/log/"
+mysql_log = "/var/log/mysql/mysql.log"
+
+# topology info
+cluster_map = "./tools/topo_example.txt"
+physicalpool_name = "pool1"
+physical_op = "create_physicalpool"
+
+# flag
+clean_before = True
+clean_after = True
+sudo_flag = True
+sudo_way = "-iu"
+
+# logicalpool info
+copyset_num = 64
+logical_op = "create_logicalpool"
+
+# libcurve workflow
+fake_mds_false = "false"
+fake_chunkserver_false = "false"
+
+#nova_host
+nova_host ="10.187.0.10"
+nova_user = "nbs"
+ssh_key = "/home/nbs/rsa/id_rsa"
+#vm_host
+vm_host = "60.191.82.99"
+vm_user = "root"
+vm_iosize = "4" #4k
+
+
+
+
+
+
---
+++ robot/Resources/swig/swig_operate.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from curvefs_python import curvefs
+from config import config
+from logger import logger
+
+
+class LibCurve:
+
+    def __init__(self):
+        rc = curvefs.Init(config.client_conf)
+        logger.info("init success.")
+        if rc != 0:
+            print ("init client fail! rc=%s" % rc)
+            logger.error("init client fail! rc=%s" % rc)
+            raise AssertionError
+
+    def libcurve_create(self, file_path, user_name, size, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Create(file_path, user_info_t, size)
+        if rc != 0:
+            print("create file fail! rc=%s" %rc)
+            logger.debug("create file fail! rc=%s" % rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+    def libcurve_open(self, file_path, user_name, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        fd = curvefs.Open(file_path, user_info_t)
+        logger.info("fd=%s" % fd)
+        return fd
+
+    def libcurve_write(self, fd, buf, offset, length):
+        rc = curvefs.Write(fd, buf, offset, length)
+        if rc < 0:
+            logger.debug("write error, rc=%s" % rc)
+            return rc
+            raise AssertionError
+        else:
+            return rc
+
+    def libcurve_read(self, fd, buf, offset, length):
+        content = curvefs.Read(fd, buf, offset, length)
+        #logger.debug(content)
+        return content
+
+    def libcurve_statfs(self, file_name, user_name, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        file_info = curvefs.FileInfo_t()
+        rc = curvefs.StatFile(file_name, user_info_t, file_info)
+        if rc == 0:
+            return file_info
+        else:
+            return rc
+            raise AssertionError
+
+    def libcurve_extend(self, file_path, user_name, new_size, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Extend(file_path, user_info_t, new_size)
+        if rc != 0:
+            logger.info("extend file fail. rc=%s" %rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+    def libcurve_close(self, fd):
+        rc = curvefs.Close(fd)
+        if rc != 0:
+            logger.info("close file fail! rc=%s" % rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+    def libcurve_rename(self, user_name, old_path, new_path, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Rename(user_info_t, old_path, new_path)
+        if rc != 0:
+            logger.info("rename file fail! rc=%s" % rc)
+            return rc
+            raise AssertionError
+        else:
+            return rc
+
+    def libcurve_delete(self, filepath, user_name, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Unlink(filepath, user_info_t)
+        if rc != 0:
+            #print "delete file fail! rc=%s" % rc
+            logger.info("delete file fail! rc=%s" % rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+    def libcurve_rmdir(self, dirpath, user_name, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Rmdir(dirpath, user_info_t)
+        if rc != 0:
+            #print "delete dir fail! rc=%s" % rc
+            logger.info("delete dir fail! rc=%s" % rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+    def libcurve_mkdir(self, dirpath, user_name, pass_word=""):
+        user_info_t = curvefs.UserInfo_t()
+        user_info_t.owner = user_name
+        user_info_t.password = pass_word
+        rc = curvefs.Mkdir(dirpath, user_info_t)
+        if rc != 0:
+            #print "mkdir fail! rc=%s" % rc
+            logger.info("mkdir fail! rc=%s" % rc)
+            return rc
+            #raise AssertionError
+        else:
+            return rc
+
+def libcurve_uninit():
+    rc = curvefs.UnInit()
+    if rc != None:
+        print "uninit  fail! rc=%s" % rc
+        logger.error("uninit file fail! rc=%s" % rc)
+        return rc
+        raise AssertionError
+    else:
+        return 0
+
+
+
+
+
