b1deb372c35be6047213ac36a797b4384dc5d9b6
tongguangxun <tongguangxun@corp.netease.com>
2019-07-18 14:04:53 +0800

    [client] 添加注册开关并附带小翠的一点修改
    Change-Id: I884b4d0ee93f1afd5516d7307e51c09241ae68d9

--- src/client/client_config.cpp
+++ src/client/client_config.cpp
@@ -11,6 +11,8 @@
 #include "src/common/string_util.h"
 #include "src/common/net_common.h"
 
+#define RETURN_IF_FALSE(x) if (x == false) return -1;
+
 namespace curve {
 namespace client {
 int ClientConfig::Init(const char* configpath) {
@@ -20,115 +22,99 @@ int ClientConfig::Init(const char* configpath) {
         return -1;
     }
 
-    if (!conf_.GetIntValue("loglevel", &fileServiceOption_.loginfo.loglevel)) {
-        LOG(ERROR) << "config no loglevel info";
-        return -1;
-    }
+    bool ret = false;
 
-    if (!conf_.GetStringValue("logpath", &fileServiceOption_.loginfo.logpath)) {
-        LOG(ERROR) << "config no logpath info";
-        return -1;
-    }
+    ret = conf_.GetIntValue("loglevel", &fileServiceOption_.loginfo.loglevel);
+    LOG_IF(ERROR, ret == false) << "config no loglevel info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt64Value("ioSplitMaxSizeKB",
-         &fileServiceOption_.ioOpt.ioSplitOpt.ioSplitMaxSizeKB)) {
-        LOG(ERROR) << "config no ioSplitMaxSizeKB info";
-        return -1;
-    }
+    ret = conf_.GetStringValue("logpath", &fileServiceOption_.loginfo.logpath);
+    LOG_IF(ERROR, ret == false) << "config no logpath info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetBoolValue("enableAppliedIndexRead",
-         &fileServiceOption_.ioOpt.ioSenderOpt.enableAppliedIndexRead)) {
-        LOG(ERROR) << "config no enableAppliedIndexRead info";
-        return -1;
-    }
+    ret = conf_.GetUInt64Value("ioSplitMaxSizeKB",
+          &fileServiceOption_.ioOpt.ioSplitOpt.ioSplitMaxSizeKB);
+    LOG_IF(ERROR, ret == false) << "config no ioSplitMaxSizeKB info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt64Value("rpcTimeoutMs",
-         &fileServiceOption_.ioOpt.ioSenderOpt.rpcTimeoutMs)) {
-        LOG(ERROR) << "config no rpcTimeoutMs info";
-        return -1;
-    }
+    ret = conf_.GetBoolValue("enableAppliedIndexRead",
+          &fileServiceOption_.ioOpt.ioSenderOpt.enableAppliedIndexRead);
+    LOG_IF(ERROR, ret == false) << "config no enableAppliedIndexRead info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("rpcRetryTimes",
-         &fileServiceOption_.ioOpt.ioSenderOpt.rpcRetryTimes)) {
-        LOG(ERROR) << "config no rpcRetryTimes info";
-        return -1;
-    }
+    ret = conf_.GetUInt64Value("rpcTimeoutMs",
+          &fileServiceOption_.ioOpt.ioSenderOpt.rpcTimeoutMs);
+    LOG_IF(ERROR, ret == false) << "config no rpcTimeoutMs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("opMaxRetry",
-        &fileServiceOption_.ioOpt.ioSenderOpt.failRequestOpt.opMaxRetry)) {
-        LOG(ERROR) << "config no opMaxRetry info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("rpcRetryTimes",
+          &fileServiceOption_.ioOpt.ioSenderOpt.rpcRetryTimes);
+    LOG_IF(ERROR, ret == false) << "config no rpcRetryTimes info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("opRetryIntervalUs",
-        &fileServiceOption_.ioOpt.ioSenderOpt.failRequestOpt.opRetryIntervalUs)) {  //  NOLINT
-        LOG(ERROR) << "config no opRetryIntervalUs info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("opMaxRetry",
+          &fileServiceOption_.ioOpt.ioSenderOpt.failRequestOpt.opMaxRetry);
+    LOG_IF(ERROR, ret == false) << "config no opMaxRetry info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt64Value("maxInFlightRPCNum",
-        &fileServiceOption_.ioOpt.ioSenderOpt.inflightOpt.maxInFlightRPCNum)) {  //  NOLINT
-        LOG(ERROR) << "config no maxInFlightRPCNum info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("opRetryIntervalUs",
+        &fileServiceOption_.ioOpt.ioSenderOpt.failRequestOpt.opRetryIntervalUs);
+    LOG_IF(ERROR, ret == false) << "config no opRetryIntervalUs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("getLeaderRetry",
-        &fileServiceOption_.ioOpt.metaCacheOpt.getLeaderRetry)) {
-        LOG(ERROR) << "config no getLeaderRetry info";
-        return -1;
-    }
+    ret = conf_.GetUInt64Value("maxInFlightRPCNum",
+        &fileServiceOption_.ioOpt.ioSenderOpt.inflightOpt.maxInFlightRPCNum);
+    LOG_IF(ERROR, ret == false) << "config no maxInFlightRPCNum info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("retryIntervalUs",
-        &fileServiceOption_.ioOpt.metaCacheOpt.retryIntervalUs)) {
-        LOG(ERROR) << "config no retryIntervalUs info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("getLeaderRetry",
+        &fileServiceOption_.ioOpt.metaCacheOpt.getLeaderRetry);
+    LOG_IF(ERROR, ret == false) << "config no getLeaderRetry info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("getLeaderTimeOutMs",
-        &fileServiceOption_.ioOpt.metaCacheOpt.getLeaderTimeOutMs)) {
-        LOG(ERROR) << "config no getLeaderTimeOutMs info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("retryIntervalUs",
+        &fileServiceOption_.ioOpt.metaCacheOpt.retryIntervalUs);
+    LOG_IF(ERROR, ret == false) << "config no retryIntervalUs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("queueCapacity",
-        &fileServiceOption_.ioOpt.reqSchdulerOpt.queueCapacity)) {
-        LOG(ERROR) << "config no queueCapacity info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("getLeaderTimeOutMs",
+        &fileServiceOption_.ioOpt.metaCacheOpt.getLeaderTimeOutMs);
+    LOG_IF(ERROR, ret == false) << "config no getLeaderTimeOutMs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("threadpoolSize",
-        &fileServiceOption_.ioOpt.reqSchdulerOpt.threadpoolSize)) {
-        LOG(ERROR) << "config no threadpoolSize info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("queueCapacity",
+        &fileServiceOption_.ioOpt.reqSchdulerOpt.queueCapacity);
+    LOG_IF(ERROR, ret == false) << "config no queueCapacity info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("refreshTimesPerLease",
-        &fileServiceOption_.leaseOpt.refreshTimesPerLease)) {
-        LOG(ERROR) << "config no refreshTimesPerLease info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("threadpoolSize",
+        &fileServiceOption_.ioOpt.reqSchdulerOpt.threadpoolSize);
+    LOG_IF(ERROR, ret == false) << "config no threadpoolSize info";
+    RETURN_IF_FALSE(ret)
+
+    ret = conf_.GetUInt32Value("refreshTimesPerLease",
+        &fileServiceOption_.leaseOpt.refreshTimesPerLease);
+    LOG_IF(ERROR, ret == false) << "config no refreshTimesPerLease info";
+    RETURN_IF_FALSE(ret)
 
     fileServiceOption_.ioOpt.reqSchdulerOpt.ioSenderOpt
     = fileServiceOption_.ioOpt.ioSenderOpt;
 
 
-    if (!conf_.GetUInt64Value("taskQueueCapacity",
-        &fileServiceOption_.ioOpt.taskThreadOpt.taskQueueCapacity)) {
-        LOG(ERROR) << "config no taskQueueCapacity info";
-        return -1;
-    }
+    ret = conf_.GetUInt64Value("taskQueueCapacity",
+        &fileServiceOption_.ioOpt.taskThreadOpt.taskQueueCapacity);
+    LOG_IF(ERROR, ret == false) << "config no taskQueueCapacity info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("taskThreadPoolSize",
-        &fileServiceOption_.ioOpt.taskThreadOpt.taskThreadPoolSize)) {
-        LOG(ERROR) << "config no taskThreadPoolSize info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("taskThreadPoolSize",
+        &fileServiceOption_.ioOpt.taskThreadOpt.taskThreadPoolSize);
+    LOG_IF(ERROR, ret == false) << "config no taskThreadPoolSize info";
+    RETURN_IF_FALSE(ret)
 
     std::string metaAddr;
-    if (!conf_.GetStringValue("metaserver_addr", &metaAddr)) {
-        LOG(ERROR) << "config no metaserver_addr info";
-        return -1;
-    }
+    ret = conf_.GetStringValue("metaserver_addr", &metaAddr);
+    LOG_IF(ERROR, ret == false) << "config no metaserver_addr info";
+    RETURN_IF_FALSE(ret)
 
     std::vector<std::string> mdsAddr;
     common::SplitString(metaAddr, "@", &mdsAddr);
@@ -141,35 +127,35 @@ int ClientConfig::Init(const char* configpath) {
         }
     }
 
-    if (!conf_.GetUInt64Value("rpcTimeoutMs",
-        &fileServiceOption_.metaServerOpt.rpcTimeoutMs)) {
-        LOG(ERROR) << "config no rpcTimeoutMs info";
-        return -1;
-    }
+    ret = conf_.GetUInt64Value("rpcTimeoutMs",
+        &fileServiceOption_.metaServerOpt.rpcTimeoutMs);
+    LOG_IF(ERROR, ret == false) << "config no rpcTimeoutMs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("rpcRetryTimes",
-        &fileServiceOption_.metaServerOpt.rpcRetryTimes)) {
-        LOG(ERROR) << "config no rpcRetryTimes info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("rpcRetryTimes",
+        &fileServiceOption_.metaServerOpt.rpcRetryTimes);
+    LOG_IF(ERROR, ret == false) << "config no rpcRetryTimes info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("retryIntervalUs",
-        &fileServiceOption_.metaServerOpt.retryIntervalUs)) {
-        LOG(ERROR) << "config no retryIntervalUs info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("retryIntervalUs",
+        &fileServiceOption_.metaServerOpt.retryIntervalUs);
+    LOG_IF(ERROR, ret == false) << "config no retryIntervalUs info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("synchronizeRPCTimeoutMS",
-        &fileServiceOption_.metaServerOpt.synchronizeRPCTimeoutMS)) {
-        LOG(ERROR) << "config no synchronizeRPCTimeoutMS info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("synchronizeRPCTimeoutMS",
+        &fileServiceOption_.metaServerOpt.synchronizeRPCTimeoutMS);
+    LOG_IF(ERROR, ret == false) << "config no synchronizeRPCTimeoutMS info";
+    RETURN_IF_FALSE(ret)
 
-    if (!conf_.GetUInt32Value("synchronizeRPCRetryTime",
-        &fileServiceOption_.metaServerOpt.synchronizeRPCRetryTime)) {
-        LOG(ERROR) << "config no synchronizeRPCRetryTime info";
-        return -1;
-    }
+    ret = conf_.GetUInt32Value("synchronizeRPCRetryTime",
+        &fileServiceOption_.metaServerOpt.synchronizeRPCRetryTime);
+    LOG_IF(ERROR, ret == false) << "config no synchronizeRPCRetryTime info";
+    RETURN_IF_FALSE(ret)
+
+    ret = conf_.GetBoolValue("registerToMDS",
+        &fileServiceOption_.commonOpt.registerToMDS);
+    LOG_IF(ERROR, ret == false) << "config no registerToMDS info";
+    RETURN_IF_FALSE(ret)
 
     return 0;
 }
