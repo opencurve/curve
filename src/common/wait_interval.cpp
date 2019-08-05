913119b0b9842cbc534d670cee01f66b495226ff
lixiaocui1 <lixiaocui1@corp.netease.com>
2019-08-05 11:42:23 +0800

    heartbeat-fix: 心跳周期内有两次心跳发送
    Change-Id: I6d4350ce4fbcab5d18e25178a265f4cdd2b286c6

---
+++ src/common/wait_interval.cpp
@@ -0,0 +1,37 @@
+/*
+ * Project: curve
+ * Created Date: 20190805
+ * Author: lixiaocui
+ * Copyright (c) 2018 netease
+ */
+
+
+#include <thread> //NOLINT
+#include <chrono> //NOLINT
+#include "src/common/wait_interval.h"
+#include "src/common/timeutility.h"
+
+namespace curve {
+namespace common {
+void WaitInterval::Init(uint64_t intervalMs) {
+    intevalMs_ = intervalMs;
+    lastSend_ = 0;
+}
+
+void WaitInterval::WaitForNextExcution() {
+    uint64_t now = TimeUtility::GetTimeofDayMs();
+    if (lastSend_ == 0) {
+        lastSend_ = TimeUtility::GetTimeofDayMs();
+    }
+
+    int rest = intevalMs_ - (now - lastSend_);
+    rest = (rest < 0) ? 0 : rest;
+    std::this_thread::sleep_for(std::chrono::milliseconds(rest));
+
+    lastSend_ = TimeUtility::GetTimeofDayMs();
+}
+
+}  // namespace common
+}  // namespace curve
+
+
