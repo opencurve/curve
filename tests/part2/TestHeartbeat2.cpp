0544df692c69b02f09c2dfbc351a1d23d2278e93
hzwuhongsong <hzwuhongsong@corp.netease.com>
2019-10-22 06:38:38 +0000

    modify tc coredump
    Change-Id: Ia5076200640d0464770b800eecd84aeae828d4de

--- tests/part2/TestHeartbeat2.cpp
+++ tests/part2/TestHeartbeat2.cpp
@@ -129,53 +129,17 @@ TEST(CloseQemuDetachedVolumes, CloseQemuDetachedVolumes_5) {
 
     EXPECT_EQ(-1, CloseQemuDetachedVolumes(0));
 }
-/*
-TEST(CloseQemuDetachedVolumes, CloseQemuDetachedVolumes_6)
-{
-    int fd = 6;
-    char* filename =
-const_cast<char*>("rbd:rbd/volume01:auth_supported=none:mon_host=10.182.30.27:6789");
-    FdImage_t fd_image;
-    fd_image.filename = filename;
 
-    int fd2 = 7;
-    char* filename2 = const_cast<char*>("mon_host=10.182.30.27");
-    FdImage_t fd_image2;
-    fd_image2.filename = filename2;
-
-    g_imageMap.clear();
-    g_imageMap.insert(std::pair<int, FdImage_t*>(fd, &fd_image));
-    g_imageMap.insert(std::pair<int, FdImage_t*>(fd2, &fd_image2));
-
-    g_qemuPoolVolumes.push_back("rbd/volume02");
-    NebdServerMocker mock;
-    EXPECT_CALL(mock, ReadQemuXmls()).WillOnce(testing::Return(0));
-    std::string lock_file = "/tmp/unit_test_lockfile";
-    EXPECT_CALL(mock, GetUuidLockfile()).WillOnce(testing::Return(lock_file));
-    EXPECT_CALL(mock, LockFile(::testing::_)).WillOnce(testing::Return(0));
-    EXPECT_CALL(mock, UnLockFile(::testing::_)).WillOnce(testing::Return());
-    EXPECT_CALL(mock, CloseImage(::testing::_)).WillOnce(testing::Return(0));
-    EXPECT_CALL(mock, RmFd(::testing::_)).WillOnce(testing::Return(0));
-
-    EXPECT_EQ(0, CloseQemuDetachedVolumes(0));
-}
-*/
 TEST(CloseQemuDetachedVolumes,
-     CloseQemuDetachedVolumes_7) {
+     CloseQemuDetachedVolumes_6) {
     int fd = 6;
     char* filename = const_cast<char*>(
         "rbd:rbd/volume01:auth_supported=none:mon_host=10.182.30.27:6789");
     FdImage_t fd_image;
     fd_image.filename = filename;
 
-    int fd2 = 7;
-    char* filename2 = const_cast<char*>("mon_host=10.182.30.27");
-    FdImage_t fd_image2;
-    fd_image2.filename = filename2;
-
     g_imageMap.clear();
     g_imageMap.insert(std::pair<int, FdImage_t*>(fd, &fd_image));
-    g_imageMap.insert(std::pair<int, FdImage_t*>(fd2, &fd_image2));
 
     g_qemuPoolVolumes.push_back("rbd/volume01");
     NebdServerMocker mock;
