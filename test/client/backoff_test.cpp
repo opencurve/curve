c582e1f43795126bfbcd5cd29f5a2597074aa406
tongguangxun <tongguangxun@corp.netease.com>
2019-10-25 21:09:13 +0800

    修复指数退避bug
    Change-Id: Ibdbd15a121ced88215f34d766aad8284ba8191da

---
+++ test/client/backoff_test.cpp
@@ -0,0 +1,111 @@
+/*
+ * Project: curve
+ * File Created: Statday, 26th Oct 2019 3:24:40 pm
+ * Author: tongguangxun
+ * Copyright (c) 2019 NetEase
+ */
+
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+#include "src/client/config_info.h"
+#include "src/client/chunk_closure.h"
+
+namespace curve {
+namespace client {
+TEST(ClientClosure, GetPowTimeTest) {
+    ASSERT_EQ(0, ClientClosure::GetPowTime(0));
+    ASSERT_EQ(0, ClientClosure::GetPowTime(1));
+    ASSERT_EQ(2, ClientClosure::GetPowTime(4));
+    ASSERT_EQ(1, ClientClosure::GetPowTime(2));
+    ASSERT_EQ(1, ClientClosure::GetPowTime(3));
+    ASSERT_EQ(2, ClientClosure::GetPowTime(7));
+    ASSERT_EQ(3, ClientClosure::GetPowTime(10));
+    ASSERT_EQ(3, ClientClosure::GetPowTime(15));
+    ASSERT_EQ(5, ClientClosure::GetPowTime(32));
+    ASSERT_EQ(5, ClientClosure::GetPowTime(63));
+    ASSERT_EQ(6, ClientClosure::GetPowTime(64));
+    ASSERT_EQ(7, ClientClosure::GetPowTime(255));
+    ASSERT_EQ(8, ClientClosure::GetPowTime(256));
+    ASSERT_EQ(8, ClientClosure::GetPowTime(257));
+    ASSERT_EQ(10, ClientClosure::GetPowTime(1024));
+    ASSERT_EQ(10, ClientClosure::GetPowTime(2047));
+    ASSERT_EQ(11, ClientClosure::GetPowTime(2048));
+    ASSERT_EQ(11, ClientClosure::GetPowTime(2049));
+}
+
+TEST(ClientClosure, OverLoadBackOffTest) {
+    FailureRequestOption_t failopt;
+    failopt.maxRetrySleepIntervalUs = 8000000;
+    failopt.opRetryIntervalUs = 500000;
+
+    ClientClosure::SetFailureRequestOption(failopt);
+
+    WriteChunkClosure cc(nullptr, nullptr);
+
+    for (int i = 1; i < 1000; i++) {
+        if (i < ClientClosure::backoffParam_.maxOverloadPow) {
+            uint64_t curTime = failopt.opRetryIntervalUs*std::pow(2, i);
+            ASSERT_LT(cc.OverLoadBackOff(i), curTime + 0.1 * curTime);
+            ASSERT_GT(cc.OverLoadBackOff(i), curTime - 0.1 * curTime);
+        } else {
+            ASSERT_LT(cc.OverLoadBackOff(i), failopt.maxRetrySleepIntervalUs +
+                                         0.1 * failopt.maxRetrySleepIntervalUs);
+            ASSERT_GT(cc.OverLoadBackOff(i), failopt.maxRetrySleepIntervalUs -
+                                         0.1 * failopt.maxRetrySleepIntervalUs);
+        }
+    }
+
+    failopt.maxRetrySleepIntervalUs = 64000000;
+    failopt.opRetryIntervalUs = 500000;
+
+    ClientClosure::SetFailureRequestOption(failopt);
+
+    for (int i = 1; i < 1000; i++) {
+        if (i < ClientClosure::backoffParam_.maxOverloadPow) {
+            uint64_t curTime = failopt.opRetryIntervalUs*std::pow(2, i);
+            ASSERT_LT(cc.OverLoadBackOff(i), curTime + 0.1 * curTime);
+            ASSERT_GT(cc.OverLoadBackOff(i), curTime - 0.1 * curTime);
+        } else {
+            ASSERT_LT(cc.OverLoadBackOff(i), failopt.maxRetrySleepIntervalUs +
+                                        0.1 * failopt.maxRetrySleepIntervalUs);
+            ASSERT_GT(cc.OverLoadBackOff(i), failopt.maxRetrySleepIntervalUs -
+                                        0.1 * failopt.maxRetrySleepIntervalUs);
+        }
+    }
+}
+
+TEST(ClientClosure, TimeoutBackOffTest) {
+    FailureRequestOption_t failopt;
+    failopt.maxTimeoutMS = 3000;
+    failopt.rpcTimeoutMs = 500;
+
+    ClientClosure::SetFailureRequestOption(failopt);
+
+    WriteChunkClosure cc(nullptr, nullptr);
+
+    for (int i = 1; i < 1000; i++) {
+        if (i < ClientClosure::backoffParam_.maxTimeoutPow) {
+            uint64_t curTime = failopt.rpcTimeoutMs*std::pow(2, i);
+            ASSERT_EQ(cc.TimeoutBackOff(i), curTime);
+        } else {
+            ASSERT_EQ(cc.TimeoutBackOff(i), 2000);
+        }
+    }
+
+    failopt.maxTimeoutMS = 4000;
+    failopt.rpcTimeoutMs = 500;
+
+    ClientClosure::SetFailureRequestOption(failopt);
+
+    for (int i = 1; i < 1000; i++) {
+        if (i < ClientClosure::backoffParam_.maxTimeoutPow) {
+            uint64_t curTime = failopt.rpcTimeoutMs*std::pow(2, i);
+            ASSERT_EQ(cc.TimeoutBackOff(i), curTime);
+        } else {
+            ASSERT_EQ(cc.TimeoutBackOff(i), 4000);
+        }
+    }
+}
+
+}   // namespace client
+}   // namespace curve
