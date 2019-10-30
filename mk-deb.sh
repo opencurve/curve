9676f44153739dc1783ba07c7bb3ef48c7799348
charisu <charisu@corp.netease.com>
2019-10-30 21:51:06 +0800

    增加copyset健康状态检查工具
    Change-Id: I810a56fff8b2be3514751638c0d70a49d261f864

--- mk-deb.sh
+++ mk-deb.sh
@@ -185,6 +185,12 @@ fi
 #	exit
 #fi
 
+cp ./bazel-bin/src/tools/curve_format \
+build/curve-chunkserver/usr/bin/curve-format
+if [ $? -ne 0 ]
+then
+	exit
+fi
 
 mkdir -p build/curve-sdk/usr/curvefs
 if [ $? -ne 0 ]
