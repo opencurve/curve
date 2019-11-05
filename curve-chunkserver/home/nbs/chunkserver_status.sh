cca2523b825a4317bc5896560e04ee889666be2d
charisu <charisu@corp.netease.com>
2019-11-05 19:31:26 +0800

    优化chunkserver-start和status脚本
    Change-Id: I2bf27bfbae8d082a44241e2fd9ef7fab7ddb03e1

--- curve-chunkserver/home/nbs/chunkserver_status.sh
+++ curve-chunkserver/home/nbs/chunkserver_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `lsblk|grep chunkserver|awk '{print $7}'|awk -F"/" '{print $3}'`
+for i in `cat /etc/fstab|grep chunkserver|awk '{print $2}'|awk -F"/" '{print $3}'`
 do
 	ps -efl|grep -w $i|grep -v grep > /dev/null 2>&1
 	if [ $? -eq 0 ]
