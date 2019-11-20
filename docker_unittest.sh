9a70f2200c685db294f3393d1484de68412d5101
majie1 <majie1@corp.netease.com>
2019-11-20 11:52:16 +0800

    update coverage statistics
    Change-Id: I1bbf5e960ac1b4f5f3299cab6e8ac84e6388bce6

--- docker_unittest.sh
+++ docker_unittest.sh
@@ -2,7 +2,14 @@
 set -x
 set -e
 
-rm -rf build coverage
+rm -rf build coverage lcov
+
+git clone https://github.com/linux-test-project/lcov.git
+cd lcov && make install && cd ..
+
+update-alternatives --install /usr/bin/gcc gcc /usr/lib/gcc-4.9-backport/bin/gcc 90\
+ --slave /usr/bin/g++ g++ /usr/lib/gcc-4.9-backport/bin/g++ \
+ --slave /usr/bin/gcov gcov /usr/lib/gcc-4.9-backport/bin/gcov
 
 ./do_cmake
 cd build
@@ -21,8 +28,6 @@ for i in ${gcda}; do
   cp --parents $j coverage
 done
 
-lcov --directory coverage$(pwd)/build --capture --output-file post.info
-lcov --directory coverage$(pwd)/build --capture --initial --output-file raw.info
-lcov -a post.info -a raw.info -o cov.info --rc lcov_branch_coverage=1
-lcov --remove cov.info '/usr/*' '*3rdparty*' '*/build/*' -o result.info
+lcov --directory coverage$(pwd)/build --capture --output-file cov.info --rc lcov_branch_coverage=1
+lcov --remove cov.info '/usr/*' '*3rdparty*' '*/build/*' -o result.info --rc lcov_branch_coverage=1
 genhtml result.info --branch-coverage --ignore-errors source -o result
