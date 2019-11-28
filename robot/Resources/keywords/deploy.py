423feb9b2e089f8f26d14772a8f09bfb88f23e7b
chenyunhui <chenyunhui@corp.netease.com>
2019-11-28 16:48:08 +0800

    增加 snap_client.conf 配置
    Change-Id: If6aa5ce2ee4aa6490baf4e89c2feee2a96906ca0

--- robot/Resources/keywords/deploy.py
+++ robot/Resources/keywords/deploy.py
@@ -139,20 +139,24 @@ def add_config():
         assert rs[3] == 0,"mv %s s3 conf fail"%host
     for host in config.snap_server_list:
         ssh = shell_operator.create_ssh_connect(host, 1046, config.abnormal_user)
-        cmd = "scp -i %s -o StrictHostKeyChecking=no -P 1046 conf/s3.conf client.conf conf/snapshot_clone_server.conf %s:~/"%\
+        cmd = "scp -i %s -o StrictHostKeyChecking=no -P 1046 conf/s3.conf client.conf conf/snapshot_clone_server.conf conf/snap_client.conf %s:~/"%\
                   (config.pravie_key_path,host)
         shell_operator.run_exec2(cmd)
-        ori_cmd = "sed -i \"s/client.config_path=\S*/client.config_path=\/etc\/curve\/client.conf/\" snapshot_clone_server.conf"
+        ori_cmd = "sed -i \"s/client.config_path=\S*/client.config_path=\/etc\/curve\/snap_client.conf/\" snapshot_clone_server.conf"
         rs = shell_operator.ssh_exec(ssh, ori_cmd)
         assert rs[3] == 0,"change host %s snapshot config fail"%host
         ori_cmd = "sed -i \"s/s3.config_path=\S*/s3.config_path=\/etc\/curve\/s3.conf/\" snapshot_clone_server.conf"
         rs = shell_operator.ssh_exec(ssh, ori_cmd)
         assert rs[3] == 0,"change host %s snapshot config fail"%host
-        ori_cmd = "sed -i \"s/server.address=\S*/server.address=%s:5555/\" snapshot_clone_server.conf"%host
+        ori_cmd = "sed -i \"s/server.address=\S*/server.address=%s:5555/g\" snapshot_clone_server.conf"%host
+        rs = shell_operator.ssh_exec(ssh, ori_cmd)
+        assert rs[3] == 0,"change host %s snapshot config fail"%host
+#change snap_client.conf
+        ori_cmd = "sed -i \"s/metaserver_addr=\S*/metaserver_addr=%s/g\" snap_client.conf"%(addrs)
         rs = shell_operator.ssh_exec(ssh, ori_cmd)
         assert rs[3] == 0,"change host %s snapshot config fail"%host
 
-        ori_cmd = "sudo mv s3.conf /etc/curve/ && sudo mv client.conf /etc/curve/ && sudo mv snapshot_clone_server.conf /etc/curve/"
+        ori_cmd = "sudo mv s3.conf /etc/curve/ && sudo mv client.conf /etc/curve/ && sudo mv snapshot_clone_server.conf /etc/curve/ && sudo mv snap_client.conf /etc/curve/"
         rs = shell_operator.ssh_exec(ssh, ori_cmd)
         assert rs[3] == 0,"mv %s snapshot_clone_server conf fail"%host
         
