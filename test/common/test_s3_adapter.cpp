20be46b17d7a6194609092da6475fffb5a2d6291
xuchaojie <xuchaojie@corp.netease.com>
2019-12-10 11:55:29 +0800

    change s3.conf use pub
    Change-Id: I3a0a979844baca1428894ce954f031941e4cd870

--- test/common/test_s3_adapter.cpp
+++ test/common/test_s3_adapter.cpp
@@ -21,7 +21,7 @@ class TestS3Adapter : public ::testing::Test {
     void SetUp() {
         adapter_ = new S3Adapter();
         adapter_->Init("./conf/s3_test.conf");
-        adapter_->SetBucketName("curve-s3-test");
+        adapter_->SetBucketName("curve-s3-utest");
     }
     void TearDown() {
         adapter_->Deinit();
