da339b60faa1f5910a4a5f7c081239e804e6d0b7
xuchaojie <xuchaojie@corp.netease.com>
2020-01-13 17:06:00 +0800

    pick from develop-snapshot
    Change-Id: Ic16cf7eced5ded5d44a9b45e80e85a6973807b47

--- src/snapshotcloneserver/clone/clone_task.h
+++ src/snapshotcloneserver/clone/clone_task.h
@@ -121,6 +121,8 @@ struct RecoverChunkContext {
     uint64_t partSize;
     // RecoverChunk请求的返回值
     int retCode;
+    // taskid
+    TaskIdType taskid;
 };
 
 using RecoverChunkContextPtr = std::shared_ptr<RecoverChunkContext>;
@@ -133,6 +135,17 @@ struct RecoverChunkClosure : public SnapCloneClosure {
     void Run() {
         std::unique_ptr<RecoverChunkClosure> self_guard(this);
         context_->retCode = GetRetCode();
+        if (context_->retCode < 0) {
+            LOG(ERROR) << "RecoverChunkClosure return fail"
+                       << ", ret = " << context_->retCode
+                       << ", logicalPoolId = "
+                       << context_->cidInfo.lpid_
+                       << ", copysetId = " << context_->cidInfo.cpid_
+                       << ", chunkId = " << context_->cidInfo.cid_
+                       << ", partIndex = " << context_->partIndex
+                       << ", partSize = " << context_->partSize
+                       << ", taskid = " << context_->taskid;
+        }
         tracker_->PushResultContext(context_);
         tracker_->HandleResponse(context_->retCode);
     }
