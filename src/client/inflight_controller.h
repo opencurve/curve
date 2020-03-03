fa9ea12d7f2cd799dae1d3e0c9e2cd141159babe
wuhanqing <wuhanqing@corp.netease.com>
2020-03-03 20:36:08 +0800

    fix condition variable wait forever
    Change-Id: Ie4a102802fbc833a31765186118ce07ef4700ad8

--- src/client/inflight_controller.h
+++ src/client/inflight_controller.h
@@ -17,80 +17,87 @@ namespace curve {
 namespace client {
 class InflightControl {
  public:
-  InflightControl() {
-    curInflightIONum_.store(0);
-  }
+    InflightControl() {
+        curInflightIONum_.store(0);
+    }
 
-  void SetMaxInflightNum(uint64_t maxInflightNum) {
-      maxInflightNum_ = maxInflightNum;
-  }
+    void SetMaxInflightNum(uint64_t maxInflightNum) {
+        maxInflightNum_ = maxInflightNum;
+    }
 
-  /**
-   * 调用该接口等待inflight全部回来，这段期间是hang的
-   */
-  void WaitInflightAllComeBack() {
-    LOG(INFO) << "wait inflight to complete, count = " << curInflightIONum_;
-    std::unique_lock<std::mutex> lk(inflightAllComeBackmtx_);
-    inflightAllComeBackcv_.wait(lk, [this]() {
-        return curInflightIONum_.load(std::memory_order_acquire) == 0;
-    });
-    LOG(INFO) << "inflight ALL come back.";
-  }
-  /**
-   * 调用该接口等待inflight回来，这段期间是hang的
-   */
-  void WaitInflightComeBack() {
-    if (curInflightIONum_.load() >= maxInflightNum_) {
-      std::unique_lock<Mutex> lk(inflightComeBackmtx_);
+    /**
+     * 调用该接口等待inflight全部回来，这段期间是hang的
+     */
+    void WaitInflightAllComeBack() {
+        LOG(INFO) << "wait inflight to complete, count = " << curInflightIONum_;
+        std::unique_lock<std::mutex> lk(inflightAllComeBackmtx_);
+        inflightAllComeBackcv_.wait(lk, [this]() {
+            return curInflightIONum_.load(std::memory_order_acquire) == 0;
+        });
+        LOG(INFO) << "inflight ALL come back.";
+    }
+
+    /**
+     * 调用该接口等待inflight回来，这段期间是hang的
+     */
+    void WaitInflightComeBack() {
+        if (curInflightIONum_.load() >= maxInflightNum_) {
+            std::unique_lock<Mutex> lk(inflightComeBackmtx_);
             inflightComeBackcv_.wait(lk, [this]() {
-            return curInflightIONum_.load() < maxInflightNum_;
-      });
+                return curInflightIONum_.load() < maxInflightNum_;
+            });
+        }
     }
-  }
-  /**
-   * 递增inflight num
-   */
-  void IncremInflightNum() {
-    curInflightIONum_.fetch_add(1, std::memory_order_release);
-  }
-  /**
-   * 递减inflight num
-   */
-  void DecremInflightNum() {
-    curInflightIONum_.fetch_sub(1, std::memory_order_release);
-    if (curInflightIONum_.load() == 0) {
-        inflightAllComeBackcv_.notify_all();
+
+    /**
+     * 递增inflight num
+     */
+    void IncremInflightNum() {
+        curInflightIONum_.fetch_add(1, std::memory_order_release);
     }
-    inflightComeBackcv_.notify_one();
-  }
 
-/**
- * WaitInflightComeBack会检查当前未返回的io数量是否超过我们限制的最大未返回inflight数量
- * 但是真正的inflight数量与上层并发调用的线程数有关。
- * 假设我们设置的maxinflight=100，上层有三个线程在同时调用GetInflightToken，
- * 如果这个时候inflight数量为99，那么并发状况下这3个线程在WaitInflightComeBack
- * 都会通过然后向下并发执行IncremInflightNum，这个时候真正的inflight为102，
- * 下一个下发的时候需要等到inflight数量小于100才能继续，也就是等至少3个IO回来才能继续
- * 下发。这个误差是可以接受的，他与scheduler一侧并发度有关，误差有上限。
- * 如果想要精确控制inflight数量，就需要在接口处加锁，让原本可以并发的逻辑变成了
- * 串行，这样得不偿失。因此我们这里选择容忍一定误差范围。
- */
-  void GetInflightToken() {
-    WaitInflightComeBack();
-    IncremInflightNum();
-  }
+    /**
+     * 递减inflight num
+     */
+    void DecremInflightNum() {
+        std::lock_guard<Mutex> lk(inflightComeBackmtx_);
+        {
+            std::lock_guard<Mutex> lk(inflightAllComeBackmtx_);
+            curInflightIONum_.fetch_sub(1, std::memory_order_release);
+            if (curInflightIONum_.load() == 0) {
+                inflightAllComeBackcv_.notify_all();
+            }
+        }
+        inflightComeBackcv_.notify_one();
+    }
 
-  void ReleaseInflightToken() {
-    DecremInflightNum();
-  }
+    /**
+     * WaitInflightComeBack会检查当前未返回的io数量是否超过我们限制的最大未返回inflight数量
+     * 但是真正的inflight数量与上层并发调用的线程数有关。
+     * 假设我们设置的maxinflight=100，上层有三个线程在同时调用GetInflightToken，
+     * 如果这个时候inflight数量为99，那么并发状况下这3个线程在WaitInflightComeBack
+     * 都会通过然后向下并发执行IncremInflightNum，这个时候真正的inflight为102，
+     * 下一个下发的时候需要等到inflight数量小于100才能继续，也就是等至少3个IO回来才能继续
+     * 下发。这个误差是可以接受的，他与scheduler一侧并发度有关，误差有上限。
+     * 如果想要精确控制inflight数量，就需要在接口处加锁，让原本可以并发的逻辑变成了
+     * 串行，这样得不偿失。因此我们这里选择容忍一定误差范围。
+     */
+    void GetInflightToken() {
+        WaitInflightComeBack();
+        IncremInflightNum();
+    }
+
+    void ReleaseInflightToken() {
+        DecremInflightNum();
+    }
 
  private:
-  uint64_t              maxInflightNum_;
-  std::atomic<uint64_t> curInflightIONum_;
-  Mutex                 inflightComeBackmtx_;
-  ConditionVariable     inflightComeBackcv_;
-  Mutex                 inflightAllComeBackmtx_;
-  ConditionVariable     inflightAllComeBackcv_;
+    uint64_t              maxInflightNum_;
+    std::atomic<uint64_t> curInflightIONum_;
+    Mutex                 inflightComeBackmtx_;
+    ConditionVariable     inflightComeBackcv_;
+    Mutex                 inflightAllComeBackmtx_;
+    ConditionVariable     inflightAllComeBackcv_;
 };
 
 }   //  namespace client
