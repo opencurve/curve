ae825565f95aa47f4e173c6dd3e44b91141af8c5
charisu <charisu@corp.netease.com>
2020-03-13 20:53:18 +0800

    copyset-check: check config when no leader
    Change-Id: I47dbea5facfabf5b6197012b7412472fa7f2f1b1

--- src/tools/curve_tool_main.cpp
+++ src/tools/curve_tool_main.cpp
@@ -39,14 +39,6 @@ const char* kHelpStr = "Usage: curve_ops_tool [Command] [OPTIONS...]\n"
 
 
 DEFINE_bool(example, false, "print the example of usage");
-DEFINE_string(mdsAddr, "127.0.0.1:6666", "mds addr");
-DEFINE_string(mdsDummyPort, "6667", "dummy port of mds, "
-                                    "can specify one or several."
-                                    "if specify several, the order "
-                                    "should be the same as mds addr");
-DEFINE_string(etcdAddr, "127.0.0.1:2379", "etcd addr");
-DEFINE_uint64(rpcTimeout, 3000, "millisecond for rpc timeout");
-DEFINE_uint64(rpcRetryTimes, 5, "rpc retry times");
 DEFINE_string(confPath, "/etc/curve/tools.conf", "config file path of tools");
 namespace brpc {
 DECLARE_int32(health_check_interval);
