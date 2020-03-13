4a9b55bac93029789747c7086863feb4d69a64a4
lixiaocui1 <lixiaocui1@corp.netease.com>
2020-03-13 17:53:28 +0800

    [fix] leader election
    Change-Id: Ic973136d59e8d841567c80b48755a6fe296a0c13

--- test/mds/mock/mock_etcdclient.h
+++ test/mds/mock/mock_etcdclient.h
@@ -30,7 +30,7 @@ class MockEtcdClient : public EtcdClientImp {
         const std::string&));
     MOCK_METHOD5(CampaignLeader, int(const std::string&, const std::string&,
         uint32_t, uint32_t, uint64_t*));
-    MOCK_METHOD3(LeaderObserve, int(uint64_t, uint64_t, const std::string&));
+    MOCK_METHOD2(LeaderObserve, int(uint64_t, const std::string&));
     MOCK_METHOD2(LeaderKeyExist, bool(uint64_t, uint64_t));
     MOCK_METHOD2(LeaderResign, int(uint64_t, uint64_t));
     MOCK_METHOD1(GetCurrentRevision, int(int64_t *));
