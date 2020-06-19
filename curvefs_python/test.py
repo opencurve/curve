59e833d15004b4c5f95a7fdd9bcf41f4cbea54f2
hzchenwei7 <hzchenwei7@corp.netease.com>
2020-06-19 16:49:48 +0800

    add curvefs tool
    Change-Id: I6eb9497cc1a05edbb69aac12d9c93df7f349c98d

--- curvefs_python/test.py
+++ curvefs_python/test.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 # coding=utf-8
+import os
+
+def exec_cmd(cmd):
+    ret = os.system(cmd)
+    if ret == 0:
+        print cmd + " exec success"
+    else :
+        print cmd + " exec fail, ret = " + str(ret)
 
 
-import curvefs
-
-ret = curvefs.Init("./client.conf");
-print ret
-
-user = curvefs.UserInfo_t()
-user.owner = "zjm"
-
-curvefs.Mkdir("/zjm", user)
-
-curvefs.Create("/zjm/test1", user, 10*1024*1024*1024)
-curvefs.Create("/zjm/test2", user, 10*1024*1024*1024)
-curvefs.Create("/zjm/test3", user, 10*1024*1024*1024)
-
-dir = curvefs.Listdir("/zjm", user)
-for i in dir:
-    print i
-
-curvefs.UnInit()
+cmd = "curve mkdir --user k8s --dirname /k8s"
+exec_cmd(cmd)
+cmd = "curve create --user k8s --filename /k8s/volume1 --length 10"
+exec_cmd(cmd)
+cmd = "curve extend --user k8s --filename /k8s/volume1 --length 11"
+exec_cmd(cmd)
+cmd = "curve rename --user k8s --filename /k8s/volume1 --newname /k8s/volume2"
+exec_cmd(cmd)
+cmd = "curve list --user k8s --dirname /k8s"
+exec_cmd(cmd)
+cmd = "curve stat --user k8s --filename /k8s/volume2"
+exec_cmd(cmd)
+cmd = "curve delete --user k8s --filename /k8s/volume2"
+exec_cmd(cmd)
+cmd = "curve rmdir --user k8s --dirname /k8s"
+exec_cmd(cmd)
