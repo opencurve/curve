c25115a72d085949369d4f6788b4a8826465e104
qinyi <qinyi01@corp.netease.com>
2020-07-01 11:04:14 +0800

    add License head for all files
    Change-Id: Ifa79d743db3cf053027265484f26cacc656b2e14

--- src/tools/nbd/interruptible_sleeper.h
+++ src/tools/nbd/interruptible_sleeper.h
@@ -1,8 +1,25 @@
 /*
+ *     Copyright (c) 2020 NetEase Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
  * Project: curve
  * Created Date: 2019-11-25
  * Author: lixiaocui
- * Copyright (c) 2018 netease
  */
 
 #ifndef SRC_TOOLS_NBD_INTERRUPTIBLE_SLEEPER_H_
