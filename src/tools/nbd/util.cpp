7256b5d9e9259c75e44870ffe0766cb2192ec83d
qinyi <qinyi01@corp.netease.com>
2020-07-01 11:04:14 +0800

    add License head for all files
    Change-Id: Ifa79d743db3cf053027265484f26cacc656b2e14

--- src/tools/nbd/util.cpp
+++ src/tools/nbd/util.cpp
@@ -1,9 +1,41 @@
 /*
+ *     Copyright (c) 2020 NetEase Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
  * Project: curve
  * Created Date: Thursday April 23rd 2020
  * Author: yangyaokai
- * Copyright (c) 2020 netease
  */
+
+/*
+ * rbd-nbd - RBD in userspace
+ *
+ * Copyright (C) 2015 - 2016 Kylin Corporation
+ *
+ * Author: Yunchuan Wen <yunchuan.wen@kylin-cloud.com>
+ *         Li Wang <li.wang@kylin-cloud.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+*/
 
 #include <linux/nbd.h>
 #include <sys/socket.h>
