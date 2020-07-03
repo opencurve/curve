4acb93febb1946caf255463f5827f026cf42843f
chenyunhui-1 <18868877340@163.com>
2020-07-03 17:49:21 +0800

    不kill test*

--- untest.sh
+++ untest.sh
@@ -19,7 +19,7 @@ git submodule update --init
 #bazel build src/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
 bazel build test/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
 for i in 0 1 2 3; do mkdir -p $i/{copysets,recycler}; done
-ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
+#ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log  & done  
 #sudo find /var/lib/jenkins/workspace/curve/curve_multijob/bazel-bin/test/ -name "*.log" | sudo xargs tar -cvf /var/lib/jenkins/log/curve_unittest/$BUILD_NUMBER/unittest.tgz
 count=2
