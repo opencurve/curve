3fc5323b154092e40824655768c9fa8946fdb363
chenyunhui-1 <18868877340@163.com>
2020-07-03 17:13:06 +0800

    test

--- untest.sh
+++ untest.sh
@@ -12,7 +12,7 @@ sudo cp /curve/curve_multijob/thirdparties/etcdclient/libetcdclient.so /usr/lib/
 #make clean
 cd /curve/curve_multijob
 bazel clean --async
-sleep 3
+sleep 5
 
 git submodule update --init
 bazel build tools/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
