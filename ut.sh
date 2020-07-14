8a1e80b838f8c2c90f3b91e379e5d15542fdc316
chenyunhui-1 <18868877340@163.com>
2020-07-14 16:41:51 +0800

    增加github CI单元测试
    Change-Id: Idb67f247d0966fc87389661cb485d85f4607fdab

--- ut.sh
+++ ut.sh
@@ -6,11 +6,13 @@ ulimit -a
 ps -ef | grep chunkserver | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 ps -ef | grep mds | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 ps -ef | grep etcd | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
+ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 
 #cd /curve/curve_multijob/thirdparties/etcdclient && make all
 #sudo cp /curve/curve_multijob/thirdparties/etcdclient/libetcdclient.so /usr/lib/
 #make clean
 cd /curve/curve_multijob
+mkdir runlog
 bazel clean --async
 sleep 5
 git submodule update --init
@@ -18,8 +20,7 @@ bazel build tools/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 
 bazel build src/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
 bazel build test/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
 for i in 0 1 2 3; do mkdir -p $i/{copysets,recycler}; done
-#ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
-for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log  & done  
+for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep -v "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log  & done  
 #sudo find /var/lib/jenkins/workspace/curve/curve_multijob/bazel-bin/test/ -name "*.log" | sudo xargs tar -cvf /var/lib/jenkins/log/curve_unittest/$BUILD_NUMBER/unittest.tgz
 count=2
 check=0
@@ -43,8 +44,8 @@ do
     f2_file=""
     now_test=`ps -ef | grep '\-test' | grep -v grep | awk '{print $8}'`
     echo "now_test case is "$now_test
-    for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do a=`cat $i.log | grep "FAILED  ]" | wc -l`;if [ $a -gt 0 ];then f1=`cat $i.log | grep "FAILED  ]"`;f1_file="${i}.log"; echo "fail test is $i"; check=1; fi;done
-    for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do b=`cat $i.log | grep "Failure" | wc -l`;if [ $b -gt 0 ];then f2=`cat $i.log | grep "Failure"`; f2_file="${i}.log";echo "fail test is $i"; check=1; fi;done
+    for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; |grep -v "integration"| grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do a=`cat $i.log | grep "FAILED  ]" | wc -l`;if [ $a -gt 0 ];then f1=`cat $i.log | grep "FAILED  ]"`;f1_file="${i}.log"; echo "fail test is $i"; check=1; fi;done
+    for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep -v "integration"|grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do b=`cat $i.log | grep "Failure" | wc -l`;if [ $b -gt 0 ];then f2=`cat $i.log | grep "Failure"`; f2_file="${i}.log";echo "fail test is $i"; check=1; fi;done
     if [ $check -eq 1 ];then
          echo "=========================test fail,Here is the logs of failed use cases========================="
          echo "=========================test fail,Here is the logs of failed use cases========================="
@@ -62,3 +63,13 @@ do
          exit -1
     fi
 done
+for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log;done 
+
+cp /root/*.py bazel-bin/
+cd bazel-bin
+./gen-coverage.py
+cp /root/check_coverage.sh ./
+bash check_coverage.sh
+cp -r coverage /curve/curve_multijob
+gcovr -x -r src -e ".*test/.*" -e ".*\.h" -e ".*usr/include/.*" -e ".*/thirdparties/*" -e "/usr/lib/*" -e ".*/external/*" -e ".*/k8-dbg/*" -e ".*/bazel_out/*" -e ".*/k8-dbg/*" -e "/usr/local/include/*" -e "test/*" -e ".*main\.cpp" -e ".*/_objs/snapshotcloneserver/*" -e ".*/_objs/mds/*" --output coverage.xml
+cp coverage.xml ../
