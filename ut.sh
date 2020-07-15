b18f69ddae6a48592b0020ef28b952a158a1c0c1
wuhanqing <wuhanqing@hotmail.com>
2020-07-15 20:13:38 +0800

    precompile etcdclient

--- ut.sh
+++ ut.sh
@@ -8,19 +8,26 @@ ps -ef | grep mds | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 ps -ef | grep etcd | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 ps -ef | grep test | grep -v grep | awk '{print $2}' | sudo xargs kill -9 || true
 
-cd /curve/curve_multijob/thirdparties/etcdclient && make all
-#sudo cp /curve/curve_multijob/thirdparties/etcdclient/libetcdclient.so /usr/lib/
-#make clean
+if [ -f /curve/etcd/libetcdclient.h ] && [ -f /curve/etcd/libetcdclient.so ]
+then
+    cp /curve/etcd/libetcdclient.h /curve/curve_multijob/thirdparties/etcdclient
+    cp /curve/etcd/libetcdclient.so /curve/curve_multijob/thirdparties/etcdclient
+else
+    cd /curve/curve_multijob/thirdparties/etcdclient && make all
+    #sudo cp /curve/curve_multijob/thirdparties/etcdclient/libetcdclient.so /usr/lib/
+    #make clean
+fi
+export LD_LIBRARY_PATH=/curve/curve_multijob/thirdparties/etcdclient:${LD_LIBRARY_PATH}
 cd /curve/curve_multijob
 mkdir runlog
 bazel clean --async
 sleep 5
 git submodule update --init
-#bazel build tools/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
-#bazel build src/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
-bazel build test/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX 
+#bazel build tools/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX
+#bazel build src/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX
+bazel build test/... --compilation_mode=dbg --collect_code_coverage  --jobs=64 --copt   -DHAVE_ZLIB=1 --define=with_glog=true --define=libunwind=true --copt -DGFLAGS_NS=google --copt -Wno-error=format-security --copt -DUSE_BTHREAD_MUTEX
 for i in 0 1 2 3; do mkdir -p $i/{copysets,recycler}; done
-for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep -v "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log  & done  
+for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep -v "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log  & done
 #sudo find /var/lib/jenkins/workspace/curve/curve_multijob/bazel-bin/test/ -name "*.log" | sudo xargs tar -cvf /var/lib/jenkins/log/curve_unittest/$BUILD_NUMBER/unittest.tgz
 count=2
 check=0
@@ -36,7 +43,7 @@ do
     echo "==========================================================================================================================================="
 
    #for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do cat $i.log;done
-    
+
     echo "==========================================================================================================================================="
     f1=""
     f2=""
@@ -63,7 +70,7 @@ do
          exit -1
     fi
 done
-for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log;done 
+for i in `find bazel-bin/test/ -type f -executable -exec file -i '{}' \; | grep  "integration" | grep  -E 'x-executable|x-sharedlib' | grep "charset=binary" | grep -v ".so"|grep test | grep -Ev 'snapshot-server|snapshot_dummy_server|client-test|server-test|multi|topology_dummy|curve_client_workflow|curve_fake_mds' | awk -F":" '{print $1'}`;do sudo $i 2>&1 | tee $i.log;done
 
 cp /root/*.py bazel-bin/
 cd bazel-bin
