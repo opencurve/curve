8a495e3ea3fb3aceac9ccdcf903db359e61d21e3
unknown <martinfounder@163.com>
2020-08-10 19:15:30 +0800

    fixed error caused by the format

--- src/mds/topology/topology.cpp
+++ src/mds/topology/topology.cpp
@@ -1170,7 +1170,7 @@ void TopologyImpl::FlushCopySetToStorage() {
     {
         ReadLockGuard rlockCopySetMap(copySetMutex_);
         for (auto &c : copySetMap_) {
-            //we only update DirtyFlag here, thus only read lock is needed
+            // we only update DirtyFlag here, thus only read lock is needed
             ReadLockGuard rlockCopySet(c.second.GetRWLockRef());
             if (c.second.GetDirtyFlag()) {
                 c.second.SetDirtyFlag(false);
@@ -1191,7 +1191,7 @@ void TopologyImpl::FlushChunkServerToStorage() {
     {
         ReadLockGuard rlockChunkServerMap(chunkServerMutex_);
         for (auto &c : chunkServerMap_) {
-            //update DirtyFlag only, thus only read lock is needed
+            // update DirtyFlag only, thus only read lock is needed
             ReadLockGuard rlockChunkServer(c.second.GetRWLockRef());
             if (c.second.GetDirtyFlag()) {
                 c.second.SetDirtyFlag(false);
