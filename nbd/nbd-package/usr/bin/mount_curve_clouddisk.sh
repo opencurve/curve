5be76ae10aa0f3a8572d03dbeebd66632f45392e
sean <seanhaizi@163.com>
2020-10-23 19:08:58 +0800

    Correct script errors

--- nbd/nbd-package/usr/bin/mount_curve_clouddisk.sh
+++ nbd/nbd-package/usr/bin/mount_curve_clouddisk.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides:          mount curve disk
+# Required-Start:    $nebd-daemon
+# Required-Stop:     
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: monut curve cloud disk
+# Description:       monut curve cloud disk for k8s calculate node
+### END INIT INFO
+
+
 #filename corresponds to the machine hostname
 #volume size 1T
 #user k8s
@@ -8,63 +19,46 @@ filelength=1024
 user=k8s
 mountpoint=/data
 
-
-#create CURVE volume
+# create CURVE volume
 volume=$(sudo curve_ops_tool list --fileName=/ | grep ${filename})
 if [ ! "$volume" ]
 then
-	sudo curve create --filename /${filename} --length ${filelength} --user ${user}
-	sudo echo -e "create CURVE volume.\nfilename: ${filename}, filelength: ${filelength}, user: ${user}" > curve_volume_info
+    sudo curve create --filename /${filename} --length ${filelength} --user ${user}
+    sudo echo -e "create CURVE volume.\nfilename: ${filename}, filelength: ${filelength}, user: ${user}" > curve_volume_info
 fi
 
-
-#map curve volume to nbd device
+# map curve volume to nbd device
 volume=$(sudo curve_ops_tool list --fileName=/ | grep ${filename})
 nbddevice=$(sudo curve-nbd list-mapped | grep ${filename})
-
 if [ ! "$volume" ]
 then
-	echo "create volume error!"
-	exit
+    echo "create volume error!"
+    exit
 elif [ ! "$nbddevice" ]
 then
-	sudo curve-nbd map cbd:pool//${filename}_${user}_
+    sudo curve-nbd map cbd:pool//${filename}_${user}_
 fi
 
-
-#format the nbd device to ext4 filesystem
+# format the nbd device to ext4 filesystem
 nbddevice=$(sudo curve-nbd list-mapped | grep ${filename} | cut -d " " -f 3)
 format=$(sudo blkid -s TYPE ${nbddevice} | awk -F '"' '{print $2}')
 if [ ! "$nbddevice" ]
 then
-	echo "map curve volume to nbd device error!"
-	exit
+    echo "map curve volume to nbd device error!"
+    exit
 elif [ "$format" != "ext4" ]
 then
-	sudo mkfs.ext4 ${nbddevice}
+    sudo mkfs.ext4 ${nbddevice}
 fi
 
-
-#mount nbd device to /data
+# mount nbd device to /data
 format=$(sudo blkid -s TYPE ${nbddevice} | awk -F '"' '{print $2}')
 hasmount=$(df -T | grep ${nbddevice})
 if [ "$format" != "ext4" ]
 then
-	echo "nbd device: ${nbddevice} format error!"
-	exit
+    echo "nbd device: ${nbddevice} format error!"
+    exit
 elif [ ! "$hasmount" ]
 then
-	sudo mount ${nbddevice} ${mountpoint}
-fi
-
-
-#restart auto mount
-UUID=$(sudo blkid -s UUID ${nbddevice} | awk -F '"' '{print $2}')
-mountpoint=$(df -T | grep ${nbddevice} | awk '{print $7}')
-filetype=$(df -T | grep ${nbddevice} | awk '{print $2}')
-hasadd=$(cat /etc/fstab | grep ${UUID})
-if [ ! "$hasadd" ]
-then
-	sudo echo -e "\n#curve nbd device: ${nbddevice}" >> /etc/fstab
-	sudo echo -e "UUID=${UUID}\t${mountpoint}\t${filetype}\tdefaults\t0\t0" >> /etc/fstab
+    sudo mount ${nbddevice} ${mountpoint}
 fi
