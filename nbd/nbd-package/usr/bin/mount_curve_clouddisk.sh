2484503e9b0c719b2ea45d03272d01df32ac3afe
sean <seanhaizi@163.com>
2020-11-03 19:21:52 +0800

    Correct k8s mount curve disk script error

--- nbd/nbd-package/usr/bin/mount_curve_clouddisk.sh
+++ nbd/nbd-package/usr/bin/mount_curve_clouddisk.sh
@@ -19,46 +19,62 @@ filelength=1024
 user=k8s
 mountpoint=/data
 
+
+# usage
+if [ "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "Usage: ./mount_curve_clouddisk.sh -- run the script to create and mount curve disk to ${mountpoint}"
+  exit 0
+fi
+
 # create CURVE volume
+echo "1. create CURVE volume"
 volume=$(sudo curve_ops_tool list --fileName=/ | grep ${filename})
 if [ ! "$volume" ]
 then
+    echo "CURVE volume info: filename: ${filename}, filelength: ${filelength}, user: ${user}"
     sudo curve create --filename /${filename} --length ${filelength} --user ${user}
-    sudo echo -e "create CURVE volume.\nfilename: ${filename}, filelength: ${filelength}, user: ${user}" > curve_volume_info
+else
+    echo "CURVE volume ${filename} exists"
 fi
 
 # map curve volume to nbd device
+echo "2. map CURVE volume to nds device"
 volume=$(sudo curve_ops_tool list --fileName=/ | grep ${filename})
 nbddevice=$(sudo curve-nbd list-mapped | grep ${filename})
 if [ ! "$volume" ]
 then
-    echo "create volume error!"
+    echo "ERROR: create CURVE volume error!"
     exit
 elif [ ! "$nbddevice" ]
 then
+    echo "curve-nbd map cbd:pool//${filename}_${user}_"
     sudo curve-nbd map cbd:pool//${filename}_${user}_
 fi
 
 # format the nbd device to ext4 filesystem
+echo "3. format the nbd device to ext4 filesystem"
 nbddevice=$(sudo curve-nbd list-mapped | grep ${filename} | cut -d " " -f 3)
 fstype=$(sudo blkid -s TYPE ${nbddevice} | awk -F '"' '{print $2}')
 if [ ! "$nbddevice" ]
 then
-    echo "map curve volume to nbd device error!"
+    echo "ERROR: map curve volume to nbd device error!"
     exit
 elif [ "$fstype" != "ext4" ]
 then
+    echo "format ${nbddevice} to ext4"
     sudo mkfs.ext4 ${nbddevice}
 fi
 
 # mount nbd device to /data
+echo "4. mount nbd device to ${mountpoint}"
 fstype=$(sudo blkid -s TYPE ${nbddevice} | awk -F '"' '{print $2}')
 hasmount=$(df -T | grep ${nbddevice})
 if [ "$fstype" != "ext4" ]
 then
-    echo "nbd device: ${nbddevice} fstype error!"
+    echo "ERROR: nbd device fstype error!"
     exit
 elif [ ! "$hasmount" ]
 then
+    echo "mount ${nbddevice} to ${mountpoint}"
     sudo mount ${nbddevice} ${mountpoint}
 fi
