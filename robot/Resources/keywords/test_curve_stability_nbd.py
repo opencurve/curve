22a02a87ad480c17de02cff2e84ddb2c7e73f383
chenyunhui-1 <18868877340@163.com>
2021-06-03 10:35:25 +0800

    add failover test case

--- robot/Resources/keywords/test_curve_stability_nbd.py
+++ robot/Resources/keywords/test_curve_stability_nbd.py
@@ -396,12 +396,13 @@ def check_clone_vol_exist(clonevol_uuid):
     return False
 
 def diff_vol_consistency(vol_uuid,clone_uuid):
-    if config.snapshot_thrash.check_md5 == True:
-        context1 = get_vol_md5(vol_uuid)
-        context2 = get_vol_md5(clone_uuid)
-        if context1 != context2:
-            logger2.error("check md5 consistency fail ,vol is %s,clone vol is %s"%(vol_uuid,clone_uuid))
-        assert context1 == context2,"vol and clone vol not same"
+    context1 = get_vol_md5(vol_uuid)
+    context2 = get_vol_md5(clone_uuid)
+    if context1 != context2:
+        logger2.error("check md5 consistency fail ,vol is %s,clone vol is %s"%(vol_uuid,clone_uuid))
+#    if config.snapshot_thrash.check_md5 == True:
+#   have added clean chunk feature 
+    assert context1 == context2,"vol and clone vol not same"
     else:
         logger2.info("do not get md5")
         return True
