49ea54c2ff049ad2349841573b6e4a91e795fc7e
chenyunhui-1 <18868877340@163.com>
2021-10-29 16:56:34 +0800

    robot:delete check_fuse_iops

--- robot/Resources/keywords/deploy.py
+++ robot/Resources/keywords/deploy.py
@@ -312,7 +312,7 @@ def destroy_curvefs():
         ret = shell_operator.run_exec(cmd)
         test_client = config.fs_test_client[0]
         ssh = shell_operator.create_ssh_connect(test_client, 1046, config.abnormal_user)
-        ori_cmd = "ps -ef|grep fuse |grep -v grep | awk '{print $2}' |sudo kill -9"
+        ori_cmd = "ps -ef|grep fuse |grep -v grep | awk '{print $2}' |sudo xargs kill -9"
         shell_operator.ssh_exec(ssh, ori_cmd)
         cmd = "cd curvefs && make stop"
         ret = shell_operator.run_exec(cmd)
