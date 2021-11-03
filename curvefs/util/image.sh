f512e79fa4778e7cba9c7ab5bea7db298da51af6
Wine93 <wine93.info@gmail.com>
2021-11-03 18:43:47 +0800

    makefile: support make image

---
+++ curvefs/util/image.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# tmpl.sh = /usr/local/metaserver.conf /tmp/metaserver.conf
+function tmpl() {
+    dsv=$1
+    src=$2
+    dst=$3
+    regex="^([^$dsv]+$dsv[[:space:]]*)(.+)__CURVEADM_TEMPLATE__[[:space:]]+(.+)[[:space:]]+__CURVEADM_TEMPLATE__(.*)$"
+    while IFS= read -r line; do
+        if [[ ! $line =~ $regex ]]; then
+            echo $line
+        else
+            echo ${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+        fi
+    done < $src > $dst
+}
+
+prefix="$(pwd)/docker/curvefs"
+mkdir -p $prefix $prefix/conf
+make install prefix="$prefix"
+make install prefix="$prefix" only=etcd
+for file in `ls conf`;
+do
+    dsv="="
+    if [ $file = "etcd.conf" ]; then
+        dsv=": "
+    fi
+
+    tmpl $dsv "conf/$file" "$prefix/conf/$file"
+done
+
+sudo docker build -t "$1" "$(pwd)/docker"
