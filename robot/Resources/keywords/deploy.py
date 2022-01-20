dcd5749adcdfcab4ec5f21e034a7418ff497e6ff
YunhuiChen <67582932+YunhuiChen@users.noreply.github.com>
2022-01-20 19:00:09 +0800

    Update deploy.py

--- robot/Resources/keywords/deploy.py
+++ robot/Resources/keywords/deploy.py
@@ -315,7 +315,7 @@ def destroy_curvefs():
         for mountpoint in config.fs_mount_dir:
             cmd = "sudo /home/nbs/.curveadm/bin/curveadm umount %s%s"%(config.fs_mount_path,mountpoint)
             shell_operator.ssh_exec(ssh, cmd)
-        cmd = "/home/nbs/.curveadm/bin/curveadm stop"
+        cmd = "echo 'yes' | /home/nbs/.curveadm/bin/curveadm stop"
         ret = shell_operator.run_exec(cmd)
         cmd = "echo 'yes' | /home/nbs/.curveadm/bin/curveadm clean"
         ret = shell_operator.run_exec(cmd)
