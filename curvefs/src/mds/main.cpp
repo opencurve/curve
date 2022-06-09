d88745e359415450038df356138ffbefd3c78fe7
wanghai01 <seanhaizi@163.com>
2022-06-09 14:03:24 +0800

    curvefs/mds: add log level

--- curvefs/src/mds/main.cpp
+++ curvefs/src/mds/main.cpp
@@ -25,8 +25,10 @@
 
 #include "curvefs/src/mds/mds.h"
 #include "src/common/configuration.h"
+#include "curvefs/src/common/dynamic_vlog.h"
 
 using ::curve::common::Configuration;
+using ::curvefs::common::FLAGS_vlog_level;
 
 DEFINE_string(confPath, "curvefs/conf/mds.conf", "mds confPath");
 DEFINE_string(mdsAddr, "127.0.0.1:6700", "mds listen addr");
@@ -35,6 +37,10 @@ void LoadConfigFromCmdline(Configuration *conf) {
     google::CommandLineFlagInfo info;
     if (GetCommandLineFlagInfo("mdsAddr", &info) && !info.is_default) {
         conf->SetStringValue("mds.listen.addr", FLAGS_mdsAddr);
+    }
+
+    if (GetCommandLineFlagInfo("v", &info) && !info.is_default) {
+        conf->SetIntValue("mds.loglevel", FLAGS_v);
     }
 }
 
@@ -47,7 +53,9 @@ int main(int argc, char **argv) {
     conf->SetConfigPath(confPath);
     LOG_IF(FATAL, !conf->LoadConfig())
         << "load mds configuration fail, conf path = " << confPath;
+    conf->GetValueFatalIfFail("mds.loglevel", &FLAGS_v);
     LoadConfigFromCmdline(conf.get());
+    FLAGS_vlog_level = FLAGS_v;
     conf->PrintConfig();
     if (FLAGS_log_dir.empty()) {
         if (!conf->GetStringValue("mds.common.logDir", &FLAGS_log_dir)) {
@@ -56,10 +64,10 @@ int main(int argc, char **argv) {
         }
     }
 
-    curvefs::mds::MDS mds;
-
     // initialize logging module
     google::InitGoogleLogging(argv[0]);
+
+    curvefs::mds::MDS mds;
 
     // initialize MDS options
     mds.InitOptions(conf);
