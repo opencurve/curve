a03727b9a5ae10ed95474dccf4a63ec7f5dd93ab
h0hmj <h0hmjcn@gmail.com>
2022-06-30 11:27:31 +0800

    curvefs/client: change log level of some fuseop function curvefs/mds: change log level of some function
    Signed-off-by: h0hmj <h0hmjcn@gmail.com>

--- curvefs/src/client/fuse_volume_client.cpp
+++ curvefs/src/client/fuse_volume_client.cpp
@@ -255,17 +255,15 @@ CURVEFS_ERROR FuseVolumeClient::FuseOpMkNod(fuse_req_t req, fuse_ino_t parent,
 CURVEFS_ERROR FuseVolumeClient::FuseOpLink(fuse_req_t req, fuse_ino_t ino,
                                      fuse_ino_t newparent, const char *newname,
                                      fuse_entry_param *e) {
-    LOG(INFO) << "FuseOpLink, ino: " << ino
-              << ", newparent: " << newparent
-              << ", newname: " << newname;
+    VLOG(1) << "FuseOpLink, ino: " << ino << ", newparent: " << newparent
+            << ", newname: " << newname;
     return FuseClient::FuseOpLink(
         req, ino, newparent, newname, FsFileType::TYPE_FILE, e);
 }
 
 CURVEFS_ERROR FuseVolumeClient::FuseOpUnlink(fuse_req_t req, fuse_ino_t parent,
                                              const char *name) {
-    LOG(INFO) << "FuseOpUnlink, parent: " << parent
-              << ", name: " << name;
+    VLOG(1) << "FuseOpUnlink, parent: " << parent << ", name: " << name;
     return RemoveNode(req, parent, name, FsFileType::TYPE_FILE);
 }
 
