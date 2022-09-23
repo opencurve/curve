ed1467b07d31f1527951c982b1275307c81588ac
wanghai01 <seanhaizi@163.com>
2022-09-23 19:57:37 +0800

    curvefs: implement setxattr interface.
    Signed-off-by: wanghai01 <seanhaizi@163.com>

--- curvefs/src/client/fuse_client.h
+++ curvefs/src/client/fuse_client.h
@@ -183,6 +183,10 @@ class FuseClient {
                                          const char* name, void* value,
                                          size_t size);
 
+    virtual CURVEFS_ERROR FuseOpSetXattr(fuse_req_t req, fuse_ino_t ino,
+                                         const char* name, const char* value,
+                                         size_t size, int flags);
+
     virtual CURVEFS_ERROR FuseOpListXattr(fuse_req_t req, fuse_ino_t ino,
                             char *value, size_t size, size_t *realSize);
 
