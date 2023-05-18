6f533ae4013b4b1835fc7559f49dd55341a7d27e
Sindweller <sindweller5530@gmail.com>
2023-05-18 21:18:57 +0800

    feat: add list chunkserver
    Signed-off-by: Sindweller <sindweller5530@gmail.com>

--- tools-v2/internal/error/error.go
+++ tools-v2/internal/error/error.go
@@ -441,6 +441,9 @@ var (
 	ErrBsQueryChunkServerRecoverStatus = func() *CmdError {
 		return NewInternalCmdError(60, "query chunkserver recover status fail, err: %s")
 	}
+	ErrBsListChunkServer = func() *CmdError {
+		return NewInternalCmdError(54, "list chunkserver fail, err: %s")
+	}
 
 	// http error
 	ErrHttpUnreadableResult = func() *CmdError {
