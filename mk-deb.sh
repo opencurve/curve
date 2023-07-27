eb6a0606c00576ee1ba6e39c29d7c23a286077e6
Hanqing Wu <wuhanqing@corp.netease.com>
2023-07-27 17:19:38 +0800

    build: add suffix `deb11` for debian11 package
    Signed-off-by: Hanqing Wu <wuhanqing@corp.netease.com>

--- mk-deb.sh
+++ mk-deb.sh
@@ -168,9 +168,9 @@ cd ${dir}/thirdparties/etcdclient &&
 cp ${dir}/thirdparties/etcdclient/libetcdclient.h ${dir}/include/etcdclient/etcdclient.h
 
 if [ $(gcc -dumpversion | awk -F'.' '{print $1}') -le 6 ]; then
-    bazelflags=''
+    bazelflags='--copt -w'
 else
-    bazelflags='--copt -faligned-new'
+    bazelflags='--copt -w --cxxopt -faligned-new'
 fi
 
 if [ "$1" = "debug" ]; then
@@ -311,19 +311,33 @@ cp -r k8s/nbd/nbd-package build/k8s-nbd-package
 mkdir -p build/k8s-nbd-package/usr/bin
 cp bazel-bin/nbd/src/curve-nbd build/k8s-nbd-package/usr/bin
 
-# step5 记录到debian包的配置文件，打包debian包
-version="Version: ${curve_version}"
-echo ${version} >>build/curve-mds/DEBIAN/control
-echo ${version} >>build/curve-sdk/DEBIAN/control
-echo ${version} >>build/curve-chunkserver/DEBIAN/control
-echo ${version} >>build/curve-tools/DEBIAN/control
-echo ${version} >>build/curve-monitor/DEBIAN/control
-echo ${version} >>build/curve-snapshotcloneserver/DEBIAN/control
-echo ${version} >>build/curve-nginx/DEBIAN/control
-echo ${version} >>build/nebd-package/DEBIAN/control
-echo ${version} >>build/k8s-nebd-package/DEBIAN/control
-echo ${version} >>build/nbd-package/DEBIAN/control
-echo ${version} >>build/k8s-nbd-package/DEBIAN/control
+#step5 记录到debian包的配置文件，打包debian包
+debian_version=$(lsb_release -cs)
+case $debian_version in
+    "bullseye")
+        suffix="+deb11"
+        ;;
+    "buster")
+        suffix="+deb10"
+        ;;
+    *)
+        suffix=""
+        ;;
+esac
+
+version="Version: ${curve_version}${suffix}"
+
+echo ${version} >> build/curve-mds/DEBIAN/control
+echo ${version} >> build/curve-sdk/DEBIAN/control
+echo ${version} >> build/curve-chunkserver/DEBIAN/control
+echo ${version} >> build/curve-tools/DEBIAN/control
+echo ${version} >> build/curve-monitor/DEBIAN/control
+echo ${version} >> build/curve-snapshotcloneserver/DEBIAN/control
+echo ${version} >> build/curve-nginx/DEBIAN/control
+echo ${version} >> build/nebd-package/DEBIAN/control
+echo ${version} >> build/k8s-nebd-package/DEBIAN/control
+echo ${version} >> build/nbd-package/DEBIAN/control
+echo ${version} >> build/k8s-nbd-package/DEBIAN/control
 
 dpkg-deb -b build/curve-mds .
 dpkg-deb -b build/curve-sdk .
