6da4893047309bd03f7103afd52436066a0821fb
Hanqing Wu <wuhanqing@corp.netease.com>
2023-07-27 17:19:38 +0800

    build: add version suffix for debian package
    Signed-off-by: Hanqing Wu <wuhanqing@corp.netease.com>

--- mk-deb.sh
+++ mk-deb.sh
@@ -16,8 +16,13 @@
 #  limitations under the License.
 #
 
-dir=`pwd`
-#step1 清除生成的目录和文件
+if ! grep -iq debian /etc/os-release; then
+    echo "$0 only support Debian"
+fi
+
+dir=$(pwd)
+
+# step1 清除生成的目录和文件
 bazel clean
 rm -rf curvefs_python/BUILD
 rm -rf curvefs_python/tmplib/
@@ -173,9 +178,9 @@ cd ${dir}
 cp ${dir}/thirdparties/etcdclient/libetcdclient.h ${dir}/include/etcdclient/etcdclient.h
 if [ `gcc -dumpversion | awk -F'.' '{print $1}'` -le 6 ]
 then
-    bazelflags=''
+    bazelflags='--copt -w'
 else
-    bazelflags='--copt -faligned-new'
+    bazelflags='--copt -w --cxxopt -faligned-new'
 fi
 
 if [ "$1" = "debug" ]
@@ -544,7 +549,9 @@ mkdir -p build/k8s-nbd-package/usr/bin
 cp bazel-bin/nbd/src/curve-nbd build/k8s-nbd-package/usr/bin
 
 #step5 记录到debian包的配置文件，打包debian包
-version="Version: ${curve_version}"
+debian_version=$(grep VERSION_ID /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+version="Version: ${curve_version}+deb${debian_version}"
+
 echo ${version} >> build/curve-mds/DEBIAN/control
 echo ${version} >> build/curve-sdk/DEBIAN/control
 echo ${version} >> build/curve-chunkserver/DEBIAN/control
