350230a2b1a2bbda794ebedb5935edca5071ef0d
Cyber-SiKu <Cyber-SiKu@outlook.com>
2023-07-31 20:47:08 +0800

    [feat] add completion
    Signed-off-by: Cyber-SiKu <Cyber-SiKu@outlook.com>

--- tools-v2/pkg/cli/curvecli.go
+++ tools-v2/pkg/cli/curvecli.go
@@ -30,13 +30,21 @@ import (
 	"github.com/spf13/viper"
 
 	cobratemplate "github.com/opencurve/curve/tools-v2/internal/utils/template"
+	"github.com/opencurve/curve/tools-v2/pkg/cli/command/completion"
 	"github.com/opencurve/curve/tools-v2/pkg/cli/command/curvebs"
 	"github.com/opencurve/curve/tools-v2/pkg/cli/command/version"
 	config "github.com/opencurve/curve/tools-v2/pkg/config"
 )
 
 func addSubCommands(cmd *cobra.Command) {
-	cmd.AddCommand(curvebs.NewCurveBsCommand())
+	completionCmd := completion.NewCompletionCommand()
+	cmd.AddCommand(
+		curvebs.NewCurveBsCommand(),
+		completionCmd,
+	)
+	
+	// build completion
+	cobratemplate.SetupCompletionCommand(completionCmd)
 }
 
 func setupRootCommand(cmd *cobra.Command) {
