ed6b9eb583c2ba0b88bae77a2d556eac18008ea6
wanghai01 <seanhaizi@163.com>
2023-09-12 14:23:13 +0800

    fix curvefs mds get metaserver
    Signed-off-by: wanghai01 <seanhaizi@163.com>

--- curvefs/src/mds/topology/topology.cpp
+++ curvefs/src/mds/topology/topology.cpp
@@ -532,15 +532,19 @@ bool TopologyImpl::GetMetaServer(MetaServerIdType metaserverId,
 bool TopologyImpl::GetMetaServer(const std::string &hostIp, uint32_t port,
                                  MetaServer *out) const {
     ReadLockGuard rlockMetaServerMap(metaServerMutex_);
+    bool find = false;
     for (auto it = metaServerMap_.begin(); it != metaServerMap_.end(); it++) {
         ReadLockGuard rlockMetaServer(it->second.GetRWLockRef());
         if (it->second.GetInternalIp() == hostIp &&
             it->second.GetInternalPort() == port) {
             *out = it->second;
-            return true;
+            find = true;
+            if (it->second.GetOnlineState() == OnlineState::ONLINE) {
+                return find;
+            }
         }
     }
-    return false;
+    return find;
 }
 
 TopoStatusCode TopologyImpl::AddPartition(const Partition &data) {
@@ -1641,6 +1645,7 @@ uint64_t TopologyImpl::GetFsNextInodeId(FsIdType fsId) {
 
 std::vector<CopySetInfo> TopologyImpl::ListCopysetInfo() const {
     std::vector<CopySetInfo> ret;
+    ReadLockGuard rlockCopySet(copySetMutex_);
     for (auto const &i : copySetMap_) {
         ret.emplace_back(i.second);
     }
