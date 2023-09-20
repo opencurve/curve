4eb32683fb5e4bd50ab42290f50cab7ceb37b63f
XR-stb <2322882096@qq.com>
2023-09-20 19:01:06 +0800

    [feat]tools-v2: add bs query volume clone-recover

---
+++ tools-v2/pkg/cli/command/curvebs/delete/volume/clone_recover/clone_recover.go
@@ -0,0 +1,154 @@
+/*
+*  Copyright (c) 2023 NetEase Inc.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+ */
+/*
+* Project: curve
+* Created Date: 2023-08-21
+* Author: setcy
+ */
+
+package clone
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	cmderror "github.com/opencurve/curve/tools-v2/internal/error"
+	cobrautil "github.com/opencurve/curve/tools-v2/internal/utils"
+	basecmd "github.com/opencurve/curve/tools-v2/pkg/cli/command"
+	"github.com/opencurve/curve/tools-v2/pkg/config"
+	"github.com/opencurve/curve/tools-v2/pkg/output"
+)
+
+const (
+	cloneRecoverExample = `$ curve bs delete volume clone-recover`
+)
+
+type CloneRecoverCmd struct {
+	basecmd.FinalCurveCmd
+	snapshotAddrs []string
+	timeout       time.Duration
+
+	user   string
+	src    string
+	dest   string
+	taskID string
+	all    bool
+	failed bool
+	status string
+}
+
+var _ basecmd.FinalCurveCmdFunc = (*CloneRecoverCmd)(nil)
+
+func (rCmd *CloneRecoverCmd) Init(cmd *cobra.Command, args []string) error {
+	snapshotAddrs, err := config.GetBsSnapshotAddrSlice(rCmd.Cmd)
+	if err.TypeCode() != cmderror.CODE_SUCCESS || len(snapshotAddrs) == 0 {
+		return err.ToError()
+	}
+	rCmd.snapshotAddrs = snapshotAddrs
+	rCmd.timeout = config.GetFlagDuration(rCmd.Cmd, config.HTTPTIMEOUT)
+	rCmd.user = config.GetBsFlagString(rCmd.Cmd, config.CURVEBS_USER)
+	rCmd.src = config.GetBsFlagString(rCmd.Cmd, config.CURVEBS_SRC)
+	rCmd.dest = config.GetBsFlagString(rCmd.Cmd, config.CURVEBS_DEST)
+	rCmd.taskID = config.GetBsFlagString(rCmd.Cmd, config.CURVEBS_TASKID)
+	rCmd.all = config.GetBsFlagBool(rCmd.Cmd, config.CURVEBS_ALL)
+	rCmd.failed = config.GetBsFlagBool(rCmd.Cmd, config.CURVEBS_FAILED)
+	if rCmd.failed {
+		rCmd.status = "5"
+	}
+	rCmd.SetHeader([]string{cobrautil.ROW_USER, cobrautil.ROW_SRC, cobrautil.ROW_TASK_ID, cobrautil.ROW_FILE, cobrautil.ROW_RESULT})
+	return nil
+}
+
+func (rCmd *CloneRecoverCmd) RunCommand(cmd *cobra.Command, args []string) error {
+	params := map[string]any{
+		cobrautil.QueryAction:      cobrautil.ActionGetCloneTaskList,
+		cobrautil.QueryType:        cobrautil.TypeCloneTask,
+		cobrautil.QueryUser:        rCmd.user,
+		cobrautil.QueryUUID:        rCmd.taskID,
+		cobrautil.QuerySource:      rCmd.src,
+		cobrautil.QueryDestination: rCmd.dest,
+		cobrautil.QueryStatus:      rCmd.status,
+		cobrautil.QueryLimit:       100,
+		cobrautil.QueryOffset:      0,
+	}
+	records := make([]map[string]string, 0)
+	for {
+		subUri := cobrautil.NewSnapshotQuerySubUri(params)
+		metric := basecmd.NewMetric(rCmd.snapshotAddrs, subUri, rCmd.timeout)
+		result, err := basecmd.QueryMetric(metric)
+		if err.TypeCode() != cmderror.CODE_SUCCESS {
+			return err.ToError()
+		}
+
+		var resp struct {
+			Code       string              `json:"Code"`
+			TaskInfos  []map[string]string `json:"TaskInfos"`
+			TotalCount int                 `json:"TotalCount"`
+		}
+		if err := json.Unmarshal([]byte(result), &resp); err != nil {
+			return err
+		}
+		if resp.Code != "0" {
+			return fmt.Errorf("get clone list fail, error code: %s", resp.Code)
+		}
+		if len(resp.TaskInfos) == 0 {
+			break
+		} else {
+			records = append(records, resp.TaskInfos...)
+			params[cobrautil.QueryOffset] = params[cobrautil.QueryOffset].(int) + params[cobrautil.QueryLimit].(int)
+		}
+	}
+
+	return nil
+}
+
+func (rCmd *CloneRecoverCmd) Print(cmd *cobra.Command, args []string) error {
+	return output.FinalCmdOutput(&rCmd.FinalCurveCmd, rCmd)
+}
+
+func (rCmd *CloneRecoverCmd) ResultPlainOutput() error {
+	return output.FinalCmdOutputPlain(&rCmd.FinalCurveCmd)
+}
+
+func (rCmd *CloneRecoverCmd) AddFlags() {
+	config.AddBsSnapshotCloneFlagOption(rCmd.Cmd)
+	config.AddHttpTimeoutFlag(rCmd.Cmd)
+	config.AddBsUserOptionFlag(rCmd.Cmd)
+	config.AddBsSrcOptionFlag(rCmd.Cmd)
+	config.AddBsDestOptionFlag(rCmd.Cmd)
+	config.AddBsTaskIDOptionFlag(rCmd.Cmd)
+	config.AddBsAllOptionFlag(rCmd.Cmd)
+	config.AddBsFailedOptionFlag(rCmd.Cmd)
+}
+
+func NewDeleteVolumeCloneCommand() *CloneRecoverCmd {
+	rCmd := &CloneRecoverCmd{
+		FinalCurveCmd: basecmd.FinalCurveCmd{
+			Use:     "clone",
+			Short:   "delete volume clone-recover tasks in curvebs cluster",
+			Example: cloneRecoverExample,
+		},
+	}
+	basecmd.NewFinalCurveCli(&rCmd.FinalCurveCmd, rCmd)
+	return rCmd
+}
+
+func NewCloneCommand() *cobra.Command {
+	return NewDeleteVolumeCloneCommand().Cmd
+}
