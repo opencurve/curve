7ea26b9f23563b3621d9c231a0d234968422319a
xuchaojie <xuchaojie@corp.netease.com>
2023-10-17 19:45:08 +0800

    add build spdk-tgt  scripts

--- util/install.sh
+++ util/install.sh
@@ -251,10 +251,12 @@ install_tools-v2() {
     g_project_name=$project_name
     project_prefix="$g_prefix/tools-v2"
     mkdir -p $project_prefix/sbin
+    mkdir -p $project_prefix/conf
     copy_file "$project_name/sbin/curve" "$project_prefix/sbin"
     copy_file "$project_name/pkg/config/curve.yaml" "$g_prefix/conf"
     copy_file "$project_name/sbin/daemon" "$project_prefix/sbin"
 }
+
 
 main() {
     get_options "$@"
