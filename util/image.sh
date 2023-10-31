04a33b1c065bd8197663f036b59367b0809939d7
Cyber-SiKu <Cyber-SiKu@outlook.com>
2023-10-31 15:28:03 +0800

    [fix] build error
    Signed-off-by: Cyber-SiKu <Cyber-SiKu@outlook.com>

--- util/image.sh
+++ util/image.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# $1: bs/fs $2: tag $3: os
+
+############################  BASIC FUNCTIONS
+msg() {
+    printf '%b' "$1" >&2
+}
+
+success() {
+    msg "\33[32m[✔]\33[0m ${1}${2}"
+}
+
+die() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+############################ FUNCTIONS
 # tmpl.sh = /usr/local/metaserver.conf /tmp/metaserver.conf
 function tmpl() {
     dsv=$1
@@ -15,7 +32,44 @@ function tmpl() {
     done < $src > $dst
 }
 
-prefix="$(pwd)/docker/curvebs"
+copy_file() {
+    cp -f "$1" "$2"
+    if [ $? -eq 0 ]; then
+        success "copy file $1 to $2 success\n"
+    else
+        die "copy file $1 to $2 failed\n"
+    fi
+}
+
+
+install_pkg() {
+    if [ $# -eq 2 ]; then
+        pkg=$1
+        make install stor=$1 prefix=$2
+    else
+        pkg=$3
+        make install stor=$1 prefix=$2 only=$3
+    fi
+
+    if [ $? -eq 0 ]; then
+        success "install $pkg success\n"
+    else
+        die "install $pkg failed\n"
+    fi
+}
+
+############################  MAIN()
+
+if [[ "$1" != "bs" && "$1" != "fs" ]]; then
+    die "\$1 must be either bs or fs\n"
+fi
+
+if [ "$1" == "bs" ]; then
+    docker_prefix="$(pwd)/docker/$3"
+else
+    docker_prefix="$(pwd)/curvefs/docker/$3"
+fi
+prefix="$docker_prefix/curve$1"
 mkdir -p $prefix $prefix/conf
 install_pkg $1 $prefix
 install_pkg $1 $prefix etcd
@@ -42,7 +96,7 @@ do
 
     # delimiter
     dsv="="
-    if [ $file = "etcd.conf" ]; then
+    if [ $file = "etcd.conf" -o $file = "curve.yaml" ]; then
         dsv=": "
     fi
 
@@ -55,5 +109,7 @@ do
     tmpl $dsv "$dir/$file" "$prefix/conf/$dst"
 done
 
-docker pull opencurvedocker/curvebs-base:debian9
-docker build -t "$1" "$(pwd)/docker"
+cp conf/client.conf $prefix/conf/curvebs-client.conf
+
+docker pull opencurvedocker/curve-base:$3
+docker build -t "$2" "$docker_prefix"
